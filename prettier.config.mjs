470a973a789b39434d26f3fd7d90a24c413cdf23
naruko-hstk <34113239+naruko-hstk@users.noreply.github.com>
2024-10-16 21:33:03 +0800

    Add prettier as formatter

---
+++ prettier.config.mjs
@@ -0,0 +1,26 @@
+/** * @see https://prettier.io/docs/en/configuration.html * @type
+{import("prettier").Config} */ const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css",
+  vueIndentScriptAndStyle: false,
+  endOfLine: "lf",
+  embeddedLanguageFormatting: "auto",
+  singleAttributePerLine: false,
+  plugins: ["prettier-plugin-tailwindcss"],
+}
+export default config
