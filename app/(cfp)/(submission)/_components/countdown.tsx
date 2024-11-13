d047b419ca8f40a2e6fb2cbfd715ab6181c92cb5
Pizza6inch <pizza6inch@gmail.com>
2024-11-13 19:27:25 +0800

    style:remove unuse

--- app/(cfp)/(submission)/_components/countdown.tsx
+++ app/(cfp)/(submission)/_components/countdown.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { motion, useAnimation } from "framer-motion";
+import { motion } from "framer-motion";
 
 type TimeLeft = {
   days: number;
