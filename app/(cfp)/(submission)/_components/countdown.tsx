57534aed5f257ad7757aa11dd0351640a13cb1cb
Dada878 <xx0932399@gmail.com>
2024-11-13 22:02:35 +0800

    style: refactor countdown component and remove unused animation

--- app/(cfp)/(submission)/_components/countdown.tsx
+++ app/(cfp)/(submission)/_components/countdown.tsx
@@ -51,21 +51,10 @@ export default function CountdownPage() {
     return num.toString().padStart(2, "0");
   }
 
-  const bounceAnimation = {
-    x: [0, 20, -10, 0], // 定義 X 軸的彈跳路徑
-    y: [0, -20, 10, 0], // 定義 Y 軸的彈跳路徑
-    transition: {
-      duration: 0.6, // 動畫持續時間
-      ease: "easeOut",
-      type: "spring", // 使用 spring 彈跳效果
-      stiffness: 100,
-    },
-  };
-
   return (
     <div className="flex h-[289px] w-[642px] flex-col justify-between lg:w-[840px]">
-      <div className="flex h-[200px] w-full flex-col items-center rounded-3xl border border-light-brown px-24 py-5">
-        <h3 className="text-normal tracking-widest text-light-brown">
+      <div className="border-light-brown flex h-[200px] w-full flex-col items-center rounded-3xl border px-24 py-5">
+        <h3 className="text-light-brown text-normal tracking-widest">
           距 離 投 稿 截 止 還 有 :
         </h3>
         <div className="mt-5 flex w-full justify-between">
@@ -77,11 +66,11 @@ export default function CountdownPage() {
                   initial={{ y: -30, opacity: 0 }}
                   animate={{ y: 0, opacity: 1 }}
                   transition={{ type: "spring", stiffness: 100 }}
-                  className="text-6xl text-light-brown"
+                  className="text-light-brown text-6xl"
                 >
                   {formatToTwoDigits(value)}
                 </motion.span>
-                <p className="text-normal font-light text-light-brown">
+                <p className="text-light-brown text-normal font-light">
                   {key.toUpperCase()}
                 </p>
               </div>
@@ -90,7 +79,7 @@ export default function CountdownPage() {
         </div>
       </div>
       <div className="flex h-[67px] w-full justify-between">
-        <div className="flex h-full w-[377px] items-center justify-between rounded-2xl border border-light-brown px-5 lg:w-[552px]">
+        <div className="border-light-brown flex h-full w-[377px] items-center justify-between rounded-2xl border px-5 lg:w-[552px]">
           {/* calender icon */}
           <Image
             src="/calender.svg"
@@ -99,7 +88,7 @@ export default function CountdownPage() {
             alt="calendar"
             className="min-w-[48px]"
           />
-          <div className="w-full justify-between pl-2 text-light-brown lg:flex lg:pl-12">
+          <div className="text-light-brown w-full justify-between pl-2 lg:flex lg:pl-12">
             <p className="font-bold">截止日期</p>
             <p>即日起至2024年1月21日(六) 23：59</p>
           </div>
@@ -107,13 +96,13 @@ export default function CountdownPage() {
         <motion.div
           onHoverStart={() => setIsHovered(true)}
           onHoverEnd={() => setIsHovered(false)}
-          className="h-full w-[246px] rounded-2xl bg-light-brown lg:w-[264]"
+          className="bg-light-brown h-full w-[246px] rounded-2xl lg:w-[264]"
         >
           <Link
             className="flex h-full w-full items-center justify-center"
             href={"#"}
           >
-            <h3 className="flex h-full w-1/2 items-center justify-center text-h3 font-bold text-red">
+            <h3 className="text-red flex h-full w-1/2 items-center justify-center text-h3 font-bold">
               立刻投稿
             </h3>
             <motion.div
