5445681fd7a3c3f8e07e5ef12bd5c5a17c25c889
nelsonGX <hi@nelsongx.com>
2024-11-15 23:45:56 +0800

    style: add tracking-wider

--- app/(cfp)/(submission)/poster/page.tsx
+++ app/(cfp)/(submission)/poster/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
 
           <section className="space-y-4">
             <h2 className="text-h2 font-extrabold">投稿說明</h2>
-            <p className="text-normal font-normal leading-loose">
+            <p className="text-normal font-normal leading-loose tracking-wider">
               海報投稿以靜態海報展出學生個人或團隊專案，作品以資訊相關領域為主，不限語言，不接受一稿多投，若為延伸作品需於投稿時載明。期望透過此方式使更多學生能有展現自我的機會，同時為會眾帶來不同形式的學習與交流。
             </p>
           </section>
@@ -23,7 +23,7 @@ export default function Page() {
             <h2 className="text-h2">投稿方式</h2>
             <div className="space-y-4">
               <h3 className="text-h3">稿件資訊</h3>
-              <ul className="ml-2 text-normal font-normal leading-loose [&>li]:relative [&>li]:pl-4 before:[&>li]:absolute before:[&>li]:left-0 before:[&>li]:top-[0.8em] before:[&>li]:-translate-y-[1em] before:[&>li]:text-xl before:[&>li]:content-['.']">
+              <ul className="ml-2 text-normal font-normal leading-loose tracking-wider [&>li]:relative [&>li]:pl-4 before:[&>li]:absolute before:[&>li]:left-0 before:[&>li]:top-[0.8em] before:[&>li]:-translate-y-[1em] before:[&>li]:text-xl before:[&>li]:content-['.']">
                 <li>題目 Project Title：必填、公開。長度請自行斟酌。</li>
                 <li>
                   摘要 Abstract：必填、公開。建議約 350
@@ -67,11 +67,11 @@ export default function Page() {
             <div className="space-y-4">
               <h3 className="text-h3">個人資料</h3>
               <div>
-                <p className="leading-loose">
+                <p className="leading-loose tracking-wider">
                   除了暱稱 /
                   名字、自我介紹被用於宣傳，其餘資訊僅需提供一人作為代表（當稿件同時有多位講者時），供議程組於聯絡時使用。
                 </p>
-                <ul className="ml-2 text-normal font-normal leading-loose [&>li]:relative [&>li]:pl-4 before:[&>li]:absolute before:[&>li]:left-0 before:[&>li]:top-[0.8em] before:[&>li]:-translate-y-[1em] before:[&>li]:text-xl before:[&>li]:content-['.']">
+                <ul className="ml-2 text-normal font-normal leading-loose tracking-wider [&>li]:relative [&>li]:pl-4 before:[&>li]:absolute before:[&>li]:left-0 before:[&>li]:top-[0.8em] before:[&>li]:-translate-y-[1em] before:[&>li]:text-xl before:[&>li]:content-['.']">
                   <li>
                     電子郵件
                     Email：必填。能讓大會聯絡的電子信箱。（注意：請您在大會的聯絡過程中，維持使用相同信箱）
@@ -96,7 +96,7 @@ export default function Page() {
             </div>
             <div className="space-y-4">
               <h3 className="text-h3">海報格式參考</h3>
-              <ul className="ml-2 text-normal font-normal leading-loose [&>li]:relative [&>li]:pl-4 before:[&>li]:absolute before:[&>li]:left-0 before:[&>li]:top-[0.8em] before:[&>li]:-translate-y-[1em] before:[&>li]:text-xl before:[&>li]:content-['.']">
+              <ul className="ml-2 text-normal font-normal leading-loose tracking-wider [&>li]:relative [&>li]:pl-4 before:[&>li]:absolute before:[&>li]:left-0 before:[&>li]:top-[0.8em] before:[&>li]:-translate-y-[1em] before:[&>li]:text-xl before:[&>li]:content-['.']">
                 <li>
                   中文名稱：字體大小範圍為 70 號字〜90
                   號字，且建議最多不超過兩行，置中對齊方式。
@@ -122,12 +122,13 @@ export default function Page() {
 
           <section className="space-y-4">
             <h2 className="text-h2">審稿方式</h2>
-            <p className="text-normal font-normal leading-loose">
+            <p className="text-normal font-normal leading-loose tracking-wider">
               稿件均經議程組形式審查，交予該領域之審稿委員審稿。審稿委員由
               SITCON 工作人員、業界人士及學界人士組成。
+              <br />
               審稿委員將針對以下項目進行篩選：
             </p>
-            <ul className="ml-2 text-normal font-normal leading-loose [&>li]:relative [&>li]:pl-4 before:[&>li]:absolute before:[&>li]:left-0 before:[&>li]:top-[0.8em] before:[&>li]:-translate-y-[1em] before:[&>li]:text-xl before:[&>li]:content-['.']">
+            <ul className="ml-2 text-normal font-normal leading-loose tracking-wider [&>li]:relative [&>li]:pl-4 before:[&>li]:absolute before:[&>li]:left-0 before:[&>li]:top-[0.8em] before:[&>li]:-translate-y-[1em] before:[&>li]:text-xl before:[&>li]:content-['.']">
               <li>
                 題材創新性：研究專案內容與方法具原創與可行性，產生貢獻或影響之潛力。
               </li>
@@ -141,7 +142,7 @@ export default function Page() {
 
           <section className="space-y-4">
             <h2 className="text-h2">CODE OF CONDUCT</h2>
-            <p className="text-normal font-normal leading-loose">
+            <p className="text-normal font-normal leading-loose tracking-wider">
               SITCON
               歡迎不同身分、來自不同背景的與會者，我們鼓勵女性、性少數與多元背景的參與者。為了讓大家都能愉快地參加
               SITCON，我們要求所有參與者閱讀年會的{" "}
@@ -157,43 +158,45 @@ export default function Page() {
 
           <section className="space-y-4">
             <h2 className="text-h2">投稿注意事項</h2>
-            <ul className="ml-2 text-normal font-normal leading-loose [&>li]:relative [&>li]:pl-4 before:[&>li]:absolute before:[&>li]:left-0 before:[&>li]:top-[0.8em] before:[&>li]:-translate-y-[1em] before:[&>li]:text-xl before:[&>li]:content-['.']">
-              <li>
-                除非內容明顯偏離講題、違反 CoC
-                或不符事實，我們不會強制要求講者對演講內容進行修正。
-              </li>
-              <li>
-                「議程題目」、「摘要」與「先備知識」將放置於官方網站與大會發行的文宣中。
-              </li>
-              <li>在截稿之前，投稿者可以隨時透過表單回函修改已送出的稿件。</li>
-              <li>
-                審稿過程不考慮稿件內容的難度。我們以稿件是否能夠吸引人、高品質、適合聽眾為考量。
-              </li>
-              <li>
-                若 SITCON
-                議程組認為您的稿件比較適合另一種形式，或者該形式的名額已經額滿、無法再增額時，我們會向您充分說明與溝通，並經您的同意後，轉換稿件類型或時間長度。
-              </li>
-              <li>
-                SITCON 2024
-                將會有多軌議程及各式場外活動同時進行，與會者可以自行決定要參與哪一場議程或活動。
-              </li>
-              <li>
-                為鼓勵投稿，大會提供投稿者一組報名邀請碼（一稿一組，每人最多一組），可優先報名參與本年會。若您的稿件被接受，年會將頒予感謝狀以表彰您的熱情付出與貢獻，並額外提供講者身分入場，屆時您可以將年會邀請碼贈與他人使用，邀請親朋好友共襄盛舉。
-              </li>
-              <li>
-                若同一稿件有多個講者，請在「暱稱 /
-                名字」欄位標示所有講者的稱呼，並在「演講大綱」詳細說明各個講者如何分配演講內容。稿件經接受者，無論講者人數，皆只提供一組可轉贈的入場票邀請碼、一份講者識別證與一份講者專屬迎賓禮，大會方保有最終發放之權力。未佩戴識別證的講者，僅能在自己的演講時，於該議程時段進入議程所在的會議廳。
-              </li>
-              <li>
+            <div>
+              <ul className="ml-2 text-normal font-normal leading-loose tracking-wider [&>li]:relative [&>li]:pl-4 before:[&>li]:absolute before:[&>li]:left-0 before:[&>li]:top-[0.8em] before:[&>li]:-translate-y-[1em] before:[&>li]:text-xl before:[&>li]:content-['.']">
+                <li>
+                  除非內容明顯偏離講題、違反 CoC
+                  或不符事實，我們不會強制要求講者對演講內容進行修正。
+                </li>
+                <li>
+                  「議程題目」、「摘要」與「先備知識」將放置於官方網站與大會發行的文宣中。
+                </li>
+                <li>在截稿之前，投稿者可以隨時透過表單回函修改已送出的稿件。</li>
+                <li>
+                  審稿過程不考慮稿件內容的難度。我們以稿件是否能夠吸引人、高品質、適合聽眾為考量。
+                </li>
+                <li>
+                  若 SITCON
+                  議程組認為您的稿件比較適合另一種形式，或者該形式的名額已經額滿、無法再增額時，我們會向您充分說明與溝通，並經您的同意後，轉換稿件類型或時間長度。
+                </li>
+                <li>
+                  SITCON 2024
+                  將會有多軌議程及各式場外活動同時進行，與會者可以自行決定要參與哪一場議程或活動。
+                </li>
+                <li>
+                  為鼓勵投稿，大會提供投稿者一組報名邀請碼（一稿一組，每人最多一組），可優先報名參與本年會。若您的稿件被接受，年會將頒予感謝狀以表彰您的熱情付出與貢獻，並額外提供講者身分入場，屆時您可以將年會邀請碼贈與他人使用，邀請親朋好友共襄盛舉。
+                </li>
+                <li>
+                  若同一稿件有多個講者，請在「暱稱 /
+                  名字」欄位標示所有講者的稱呼，並在「演講大綱」詳細說明各個講者如何分配演講內容。稿件經接受者，無論講者人數，皆只提供一組可轉贈的入場票邀請碼、一份講者識別證與一份講者專屬迎賓禮，大會方保有最終發放之權力。未佩戴識別證的講者，僅能在自己的演講時，於該議程時段進入議程所在的會議廳。
+                </li>
+              </ul>
+              <p className="text-normal font-normal leading-loose tracking-wider">
                 若有任何問題或投稿建議，請來信 SITCON 議程組信箱
                 session@sitcon.org。
-              </li>
-            </ul>
+              </p>
+            </div>
           </section>
 
           <section className="space-y-4">
             <h2 className="text-h2">授權</h2>
-            <p className="text-normal font-normal leading-loose">
+            <p className="text-normal font-normal leading-loose tracking-wider">
               投稿作者同意以{" "}
               <Link
                 href={"https://creativecommons.org/licenses/by/4.0/"}
@@ -208,9 +211,10 @@ export default function Page() {
 
           <section className="space-y-4">
             <h2 className="text-h2">試講</h2>
-            <p className="text-normal font-normal leading-loose">
+            <p className="text-normal font-normal leading-loose tracking-wider">
               請以直版寬841 mm、高 1189 mm （A0 大小）規格製作為 pdf
               檔案，一篇投稿一面看板，請於收到錄取通知後依據郵件說明上傳最終版海報檔案，統一由大會進行印製與佈展。
+              <br />
               佈展方式與地點：地點於 2 樓梯廳，其餘細節將於稿件錄取後通知。
             </p>
           </section>
