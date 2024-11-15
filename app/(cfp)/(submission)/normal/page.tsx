a149699c4f28f079f39a189d205bab0a52fa120b
Pizza6inch <pizza6inch@gmail.com>
2024-11-15 16:00:24 +0800

    style:removed unused

--- app/(cfp)/(submission)/normal/page.tsx
+++ app/(cfp)/(submission)/normal/page.tsx
@@ -1,14 +1,3 @@
-import TableOfContent from "@/app/(cfp)/(submission)/_components/table-of-content";
-
 export default function Page() {
-  return (
-    // <></>
-    <div className="relative h-[2000px] w-full">
-      <TableOfContent theme="light" />
-      <div className="h-[500px]"></div>
-      <section id="important-dates" className="bg-none">
-        important Dates
-      </section>
-    </div>
-  );
+  return <p>一般議程</p>;
 }
