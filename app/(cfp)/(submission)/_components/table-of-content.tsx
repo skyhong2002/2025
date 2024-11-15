a4cbbd0e9389e136378a796bef96726e2175852f
Pizza6inch <pizza6inch@gmail.com>
2024-11-15 11:02:10 +0800

    feat:complete navTable

--- app/(cfp)/(submission)/_components/table-of-content.tsx
+++ app/(cfp)/(submission)/_components/table-of-content.tsx
@@ -1,3 +1,33 @@
+import Link from "next/link";
+
 export default function TableOfContent() {
-  return <p>本頁目錄</p>;
+  const sections = [
+    { id: "important-dates", title: "重要時程" },
+    { id: "session-types", title: "議程種類" },
+    { id: "submission-topics", title: "投稿主題範例" },
+    { id: "submission-format", title: "投稿格式" },
+    { id: "review-process", title: "審稿方式" },
+    { id: "code-of-conduct", title: "Code of Conduct" },
+    { id: "submission-guidelines", title: "投稿注意事項" },
+    { id: "license", title: "授權" },
+    { id: "practice-talk", title: "試講" },
+    { id: "rehearsal", title: "彩排" },
+    { id: "qa", title: "QA" },
+  ];
+
+  return (
+    <nav className="absolute right-0 top-6 w-min">
+      <h2 className="mb-3 font-medium">本頁目錄</h2>
+      <div className="mb-3 h-[1px] w-[143px] bg-light-brown lg:w-[264px]" />
+      <ul>
+        {sections.map((section) => (
+          <li key={section.id} className="tableContentItem">
+            <Link href={`#${section.id}`} className="w-full">
+              {section.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
