c893b0fca3b886b0f504834802477b1a56b1cd93
nelsonGX <hi@nelsongx.com>
2024-11-16 02:22:34 +0800

    feat: add icons

--- app/(cfp)/(submission)/_components/timeline.tsx
+++ app/(cfp)/(submission)/_components/timeline.tsx
@@ -1,16 +1,40 @@
 export default function Timeline() {
   const events = [
-    { date: "11.12.2024", icon: Flag, text: "開始徵稿" },
-    { date: "01.21", icon: Calendar, text: "投稿截止" },
-    { date: "一月下旬", icon: Mail, text: "議程錄取通知" },
-    { date: "二月上旬至下旬", icon: Mic, text: "試講" },
-    { date: "03.07", icon: PlaySquare, text: "彩排" },
-    { date: "03.08", icon: Users, text: "年會" },
+    {
+      date: "11.12.2024",
+      icon: <span className="material-icons">flag</span>,
+      text: "開始徵稿",
+    },
+    {
+      date: "01.21",
+      icon: <span className="material-icons">flag</span>,
+      text: "投稿截止",
+    },
+    {
+      date: "一月下旬",
+      icon: <span className="material-icons">flag</span>,
+      text: "議程錄取通知",
+    },
+    {
+      date: "二月上旬至下旬",
+      icon: <span className="material-icons">flag</span>,
+      text: "試講",
+    },
+    {
+      date: "03.07",
+      icon: <span className="material-icons">flag</span>,
+      text: "彩排",
+    },
+    {
+      date: "03.08",
+      icon: <span className="material-icons">flag</span>,
+      text: "年會",
+    },
   ];
 
   return (
-    <div className="w-full bg-[#535AA9] p-8">
-      <h2 className="mb-12 text-2xl text-[#E6D5B0]">重要時程</h2>
+    <div className="w-full p-8">
+      <h2 className="mb-12 text-2xl">重要時程</h2>
       <div className="relative flex flex-col">
         <div className="relative mb-8 h-8">
           <svg className="h-8 w-full" viewBox="0 0 800 32">
@@ -42,13 +66,9 @@ export default function Timeline() {
         <div className="flex px-4">
           {events.map((event, index) => (
             <div key={index} className="flex flex-1 flex-col items-center">
-              <div className="mb-4">
-                <event.icon className="h-6 w-6 text-[#E6D5B0]" />
-              </div>
-              <div className="mb-2 text-sm text-[#E6D5B0]">{event.date}</div>
-              <div className="text-center text-sm text-[#E6D5B0]">
-                {event.text}
-              </div>
+              <div className="mb-4">{event.icon}</div>
+              <div className="mb-2 text-sm">{event.date}</div>
+              <div className="text-center text-sm">{event.text}</div>
             </div>
           ))}
         </div>
