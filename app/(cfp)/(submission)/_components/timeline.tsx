0290a5e227769c11c58aaceb838712b4b0854ccb
nelsonGX <hi@nelsongx.com>
2024-11-16 20:57:43 +0800

    feat: make the timeline

--- app/(cfp)/(submission)/_components/timeline.tsx
+++ app/(cfp)/(submission)/_components/timeline.tsx
@@ -1,10 +1,10 @@
 export default function Timeline() {
   return (
-    <div className="max-w-[840px] p-8">
-      <h2 className="mb-12 text-h2">重要時程</h2>
+    <div className="max-w-[840px] p-8 mx-auto">
+      <h2 className="mb-2 text-h2">重要時程</h2>
       <div className="relative flex flex-col">
-        <div className="relative mb-8 h-8">
-          <svg className="h-8 w-full" viewBox="0 0 800 32">
+        <div className="absolute mb-8 h-8">
+          <svg className="h-8 max-w-full" viewBox="0 0 800 32">
             {/* Main horizontal line */}
             <line
               x1="20"
@@ -12,58 +12,77 @@ export default function Timeline() {
               x2="740"
               y2="16"
               stroke="#D3C4B7"
-              strokeWidth="2"
+              strokeWidth="3.5"
             />
             
             {/* Arrow */}
             <path
               d="M740 16 L760 16 M750 8 L760 16 L750 24"
               stroke="#D3C4B7"
-              strokeWidth="2"
+              strokeWidth="3.5"
               fill="none"
             />
           </svg>
         </div>
 
         {/* 時程 */}
-        <div className="flex">
-          <div className="bg-black">
-            <svg className="h-10">
-              <circle key={1} cx={10} cy="16" r="6" fill="#CC9232" />
+        <div className="flex tracking-wider z-10 justify-between">
+
+          <div className="w-min">
+            <svg className="h-10 w-10">
+              <circle key={1} cx={10} cy="16" r="8" fill="#CC9232" />
             </svg>
-            <span className="material-icons-big text-4xl" >flag</span>
+            <span className="material-icons-big">flag</span>
+            <p>11.12.2024</p>
+            <h3 className="text-h3 w-max">開始徵稿</h3>
           </div>
 
-      {/* date: "11.12.2024",
-      icon: ,
-      text: "開始徵稿",
-    },
-    {
-      date: "01.21",
-      icon: <span className="material-icons text-red">event_busy</span>,
-      text: "投稿截止",
-    },
-    {
-      date: "一月下旬",
-      icon: <span className="material-icons">email</span>,
-      text: "議程錄取通知",
-    },
-    {
-      date: "二月上旬至下旬",
-      icon: <span className="material-icons">mic_none</span>,
-      text: "試講",
-    },
-    {
-      date: "03.07",
-      icon: <span className="material-icons">route</span>,
-      text: "彩排",
-    },
-    {
-      date: "03.08",
-      icon: <span className="material-icons text-orange">groups</span>,
-      text: "年會",
-    }, */}
+          <div className="w-min">
+            <svg className="h-10 w-10">
+              <circle key={1} cx={10} cy="16" r="8" fill="#CC9232" />
+            </svg>
+            <span className="material-icons-big text-red">event_busy</span>
+            <p>01.21</p>
+            <h3 className="text-h3 w-max">投稿截止</h3>
+          </div>
 
+          <div className="w-min">
+            <svg className="h-10 w-10">
+              <circle key={1} cx={10} cy="16" r="8" fill="#CC9232" />
+            </svg>
+            <span className="material-icons-big">email</span>
+            <p>一月下旬</p>
+            <h3 className="text-h3 w-max">議程錄取通知</h3>
+          </div>
+
+          <div className="w-min">
+            <svg className="h-10 w-10">
+              <circle key={1} cx={10} cy="16" r="8" fill="#CC9232" />
+            </svg>
+            <span className="material-icons-big">mic_none</span>
+            <p className="w-max">二月上旬至下旬</p>
+            <h3 className="text-h3 w-max">試講</h3>
+          </div>
+
+          <div className="w-min">
+            <svg className="h-10 w-10">
+              <circle key={1} cx={10} cy="16" r="8" fill="#CC9232" />
+            </svg>
+            <span className="material-icons-big">route</span>
+            <p>03.07</p>
+            <h3 className="text-h3 w-max">彩排</h3>
+          </div>
+
+          <div className="w-min">
+            <svg className="h-10 w-10">
+              <circle key={1} cx={10} cy="16" r="8" fill="#CC9232" />
+            </svg>
+            <span className="material-icons-big text-orange">groups</span>
+            <p>03.08</p>
+            <h3 className="text-h3 w-max">年會</h3>
+          </div>
+
+          <div className="px-2" />
         </div>
       </div>
     </div>
