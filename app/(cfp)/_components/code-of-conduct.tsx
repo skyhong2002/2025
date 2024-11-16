dec328b12a2678b3371ee480e06669b34ad2f2c3
naruko_hstk <34113239+naruko-hstk@users.noreply.github.com>
2024-11-16 19:43:03 +0800

    feat: coc text update COC排版更新

--- app/(cfp)/_components/code-of-conduct.tsx
+++ app/(cfp)/_components/code-of-conduct.tsx
@@ -1,79 +1,96 @@
+import Link from "next/link";
 export default function CodeOfConduct() {
   return (
     <div>
-      <div className="mb-16">
-        <h2 className="text-h2-mobile md:text-h2">CODE OF CONDUCT</h2>
-        <p className="text-normal-mobile md:text-normal">
+      <section className="space-y-4">
+        <h2 className="text-h2-mobile font-extrabold md:text-h2">
+          CODE OF CONDUCT
+        </h2>
+        <p className="text-normal-mobile font-normal leading-loose tracking-wider md:text-normal">
           SITCON
           歡迎不同身分、來自不同背景的與會者，我們鼓勵女性、性少數與多元背景的參與者。為了讓大家都能愉快地參加
-          SITCON，我們要求所有參與者閱讀年會的 行為準則（Code of
-          Conduct），共同創造一個友善的環境。
+          SITCON，我們要求所有參與者閱讀年會的{" "}
+          <Link
+            href={"https://sitcon.org/code-of-conduct/"}
+            className="text-orange underline underline-offset-2"
+          >
+            行為準則（Code of Conduct）
+          </Link>
+          ，共同創造一個友善的環境。
         </p>
-      </div>
-      <div className="mb-16">
-        <h2 className="text-h2-mobile md:text-h2">投稿注意事項</h2>
-        <p className="text-normal-mobile md:text-normal">
-          除非內容明顯偏離講題、違反 CoC
-          或不符事實，我們不會強制要求講者對演講內容進行修正。
-          「議程題目」、「摘要」與「先備知識」將放置於官方網站與大會發行的文宣中。
-          在截稿之前，投稿者可以隨時透過表單回函修改已送出的稿件。
-          審稿過程不考慮稿件內容的難度。我們以稿件是否能夠吸引人、高品質、適合聽眾為考量。
-          若 SITCON
-          議程組認為您的稿件比較適合另一種形式，或者該形式的名額已經額滿、無法再增額時，我們會向您充分說明與溝通，並經您的同意後，轉換稿件類型或時間長度。
-          SITCON 2024
-          將會有多軌議程及各式場外活動同時進行，與會者可以自行決定要參與哪一場議程或活動。
-          為鼓勵投稿，大會提供投稿者一組報名邀請碼（一稿一組，每人最多一組），可優先報名參與本年會。若您的稿件被接受，年會將頒予感謝狀以表彰您的熱情付出與貢獻，並額外提供講者身分入場，屆時您可以將年會邀請碼贈與他人使用，邀請親朋好友共襄盛舉。
-          若同一稿件有多個講者，請在「暱稱 /
-          名字」欄位標示所有講者的稱呼，並在「演講大綱」詳細說明各個講者如何分配演講內容。稿件經接受者，無論講者人數，皆只提供一組可轉贈的入場票邀請碼、一份講者識別證與一份講者專屬迎賓禮，大會方保有最終發放之權力。未佩戴識別證的講者，僅能在自己的演講時，於該議程時段進入議程所在的會議廳。
-          若有任何問題或投稿建議，請來信 SITCON 議程組信箱 session@sitcon.org。
-        </p>
-      </div>
-      <div className="mb-16">
-        <h2 className="text-h2-mobile md:text-h2">授權</h2>
-        <p className="text-normal-mobile md:text-normal">
-          今年將採取實體與線上同步之模式進行，除開放式議程外，年會當天將對每一場議程進行直播串流。如果您同意以
-          創用 CC「姓名標示」4.0 授權釋出議程錄影，我們將會上傳至 YouTube 及
-          LBRY 的 SITCON 頻道。
-          您為議程準備的其餘相關素材（如投影片），將於年會前由 SITCON
-          議程組向您蒐集素材連結，並整理在年會官方網站的議程資訊上。您可以自行決定在何種平台、以何種方式釋出，我們不會干涉，但鼓勵您選擇允許知識共享的
-          創用 CC 系列授權。
-        </p>
-      </div>
-      <div className="mb-16">
-        <h2 className="text-h2-mobile md:text-h2">試講</h2>
-        <p className="text-normal-mobile md:text-normal">
-          試講提供講者提升演講品質的機會，比照年會時間安排供講者講完整場演講，並有議程組人員提供建議（如簡報、臺風等），講者可以自由參加，並自行選擇最合適的場次。若您的稿件確認入選，議程組將再與您協調確切時間。為了維護所有講者的權利，試講時間恕不接受更改，請各位講者準時參與。
-          所有場次將依報名情況舉辦，預計舉辦北部、中部、南部、線上場等，時間約為二月上旬至二月下旬，地點另行通知。
-        </p>
+      </section>
+
+      <section className="space-y-4">
+        <h2 className="text-h2-mobile font-extrabold md:text-h2">
+          投稿注意事項
+        </h2>
         <div>
-          <p>
-            試講交通補助：為確保年會議程品質，大會鼓勵講者參與試講，並將依照客運價格標準，補助講者至最近試講場地的全額或部分車資。屆時若有需要，請保留試講當天的乘車票據，並於
-            3/8（六）年會當天繳交給工作人員。
+          <ul className="ml-2 text-normal-mobile font-normal leading-loose tracking-wider md:text-normal [&>li]:relative [&>li]:pl-4 before:[&>li]:absolute before:[&>li]:left-0 before:[&>li]:top-[0.8em] before:[&>li]:-translate-y-[1em] before:[&>li]:text-xl before:[&>li]:content-['.']">
+            <li>
+              除非內容明顯偏離講題、違反 CoC
+              或不符事實，我們不會強制要求講者對演講內容進行修正。
+            </li>
+            <li>
+              「議程題目」、「摘要」與「先備知識」將放置於官方網站與大會發行的文宣中。
+            </li>
+            <li>在截稿之前，投稿者可以隨時透過表單回函修改已送出的稿件。</li>
+            <li>
+              審稿過程不考慮稿件內容的難度。我們以稿件是否能夠吸引人、高品質、適合聽眾為考量。
+            </li>
+            <li>
+              若 SITCON
+              議程組認為您的稿件比較適合另一種形式，或者該形式的名額已經額滿、無法再增額時，我們會向您充分說明與溝通，並經您的同意後，轉換稿件類型或時間長度。
+            </li>
+            <li>
+              SITCON 2024
+              將會有多軌議程及各式場外活動同時進行，與會者可以自行決定要參與哪一場議程或活動。
+            </li>
+            <li>
+              為鼓勵投稿，大會提供投稿者一組報名邀請碼（一稿一組，每人最多一組），可優先報名參與本年會。若您的稿件被接受，年會將頒予感謝狀以表彰您的熱情付出與貢獻，並額外提供講者身分入場，屆時您可以將年會邀請碼贈與他人使用，邀請親朋好友共襄盛舉。
+            </li>
+            <li>
+              若同一稿件有多個講者，請在「暱稱 /
+              名字」欄位標示所有講者的稱呼，並在「演講大綱」詳細說明各個講者如何分配演講內容。稿件經接受者，無論講者人數，皆只提供一組可轉贈的入場票邀請碼、一份講者識別證與一份講者專屬迎賓禮，大會方保有最終發放之權力。未佩戴識別證的講者，僅能在自己的演講時，於該議程時段進入議程所在的會議廳。
+            </li>
+          </ul>
+          <p className="text-normal-mobile font-normal leading-loose tracking-wider md:text-normal">
+            若有任何問題或投稿建議，請來信 SITCON 議程組信箱
+            session@sitcon.org。
           </p>
         </div>
-      </div>
-      <div className="mb-16">
-        <h2 className="text-h2-mobile md:text-h2">彩排</h2>
-        <p className="text-normal-mobile md:text-normal">
-          本屆 SITCON
-          將在年會前一天提供入選講者彩排的機會。彩排能讓講者熟悉現場設備環境、攝影機位置，並且測試
-          Live Demo 情境。希望講者能夠共同參與彩排，一起使年會議程品質更臻完美。
-          日期：2025 年 3 月 7 日（五）。
-          場地：使用場地與正式年會相同，細節將於稿件錄取後通知。 時間：一人約
-          5~10 分鐘。若有 Live Demo 或特殊需求，可提前與議程組安排時間。
-          流程：講者操作設備，測試 Live Demo
-          連結及切換方式，也可以演練部分簡報內容。
-          提供設備：與年會當天場地的設備相同，例如麥克風、倒數計時器等。
+      </section>
+
+      <section className="space-y-4">
+        <h2 className="text-h2-mobile font-extrabold md:text-h2">授權</h2>
+        <p className="text-normal-mobile font-normal leading-loose tracking-wider md:text-normal">
+          投稿作者同意以{" "}
+          <Link
+            href={"https://creativecommons.org/licenses/by/4.0/"}
+            className="underline underline-offset-2"
+          >
+            創用 CC「姓名標示」4.0
+          </Link>{" "}
+          授權大會方進行紙本印刷、宣傳、展覽、數位化、重製等審稿、佈展、官方網站上線工作必要流程。投稿作者仍擁有投稿專案著作之著作權。且擔保本著作係著作人之原創性著作，未侵害任何第三人之智慧財產權。
         </p>
-      </div>
-      <div className="mb-16">
-        <h2 className="text-h2-mobile md:text-h2">Q&A</h2>
+        <div className="py-1" />
+      </section>
+
+      <section className="space-y-4">
+        <h2 className="text-h2-mobile font-extrabold md:text-h2">試講</h2>
+        <p className="text-normal-mobile font-normal leading-loose tracking-wider md:text-normal">
+          請以直版寬841 mm、高 1189 mm （A0 大小）規格製作為 pdf
+          檔案，一篇投稿一面看板，請於收到錄取通知後依據郵件說明上傳最終版海報檔案，統一由大會進行印製與佈展。
+          <br />
+          佈展方式與地點：地點於 2 樓梯廳，其餘細節將於稿件錄取後通知。
+        </p>
+      </section>
+
+      <section className="space-y-4">
+        <h2 className="text-h2-mobile font-extrabold md:text-h2">Q&A</h2>
         <div className="rounded-full bg-light-brown px-6 py-4 text-red">
-          Q1. tst
+          Q1. Placeholder
         </div>
-        {/* TODO: `Q${index}. ${content} */}
-        <div>{/* TODO: query from array */}</div>
-      </div>
+      </section>
     </div>
   );
 }
