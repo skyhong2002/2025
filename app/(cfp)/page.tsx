6afd9f667d552eb24e61eda2d2e64d33b7cc002b
Dada878 <xx0932399@gmail.com>
2024-11-17 14:22:56 +0800

    refactor: improve code readability

--- app/(cfp)/page.tsx
+++ app/(cfp)/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
   return (
     <>
       <div
-        className="relative h-[calc(100svh-64px)] min-h-[600px] bg-white bg-cover bg-[left_20%_center] max-[820px]:bg-gray max-sm:h-[calc(100svh-140px)] lg:bg-center"
+        className="max-[820px]:bg-gray relative h-[calc(100svh-64px)] min-h-[600px] bg-white bg-cover bg-[left_20%_center] max-sm:h-[calc(100svh-140px)] lg:bg-center"
         style={{
           backgroundImage: `url('')`,
         }}
@@ -154,13 +154,13 @@ export default function Home() {
             <div className="flex gap-4 pt-14 max-[820px]:grid max-[820px]:grid-cols-3 max-[820px]:gap-5 max-sm:grid-cols-2">
               <Link
                 href="https://groups.google.com/g/sitcon-general/"
-                className="w-[140px] rounded-[50px] bg-gray py-3 text-center font-bold max-[820px]:col-span-2 max-[820px]:w-auto max-[820px]:rounded-lg max-[820px]:text-[18px] max-[712px]:my-auto max-[712px]:text-[14px] max-sm:col-span-1 max-sm:rounded-[72px] max-sm:text-[18px] max-sm:font-normal"
+                className="bg-gray w-[140px] rounded-[50px] py-3 text-center font-bold max-[820px]:col-span-2 max-[820px]:w-auto max-[820px]:rounded-lg max-[820px]:text-[18px] max-[712px]:my-auto max-[712px]:text-[14px] max-sm:col-span-1 max-sm:rounded-[72px] max-sm:text-[18px] max-sm:font-normal"
               >
                 訂閱郵件論壇
               </Link>
               <Link
                 href="https://gitlab.com/sitcon-tw/2024/board"
-                className="w-[140px] rounded-[50px] bg-gray py-3 text-center font-bold max-[820px]:col-span-1 max-[820px]:w-auto max-[820px]:rounded-lg max-[820px]:text-[18px] max-[712px]:my-auto max-[712px]:text-[14px] max-sm:rounded-[72px] max-sm:text-[18px] max-sm:font-normal"
+                className="bg-gray w-[140px] rounded-[50px] py-3 text-center font-bold max-[820px]:col-span-1 max-[820px]:w-auto max-[820px]:rounded-lg max-[820px]:text-[18px] max-[712px]:my-auto max-[712px]:text-[14px] max-sm:rounded-[72px] max-sm:text-[18px] max-sm:font-normal"
               >
                 查看 GitLab
               </Link>
@@ -171,204 +171,136 @@ export default function Home() {
       </div>
 
       {/* Desktop Layout */}
-      <div className="relative grid grid-cols-3 gap-4 bg-light-brown bg-cover bg-[left_20%_center] px-10 max-[820px]:hidden lg:bg-center">
-        <div className="col-span-1 h-auto">
-          <div className="rounded-[32px] border-2 border-blue bg-white p-6">
-            <div>
-              <h1 className="text-h1 font-black tracking-[2px] text-blue">
-                支持我們
-              </h1>
-              <p className="text-justify text-normal leading-7 text-blue">
-                SITCON
-                秉持著不向與會者收費的原則，十年以來致力於推廣學生資訊教育，創造學生交流、教學互長的機會。
-                <br />
-                <br />
-                如果您也認同 SITCON 的理念，歡迎以行動支持 SITCON
-                的運作，點擊下方「個人贊助方案」，您的支持將會成為我們的動力，讓更多學生能在舞台上展現自我、讓資訊教育遍及臺灣的每個角落！
-                <br />
-                <br />
-                若您有興趣成為我們的贊助夥伴，歡迎點擊下方「索取贊助徵求書」，了解詳細合作方案，或是利用
-                contact@sitcon.org 聯絡我們，商討更多的合作方式！
-              </p>
-            </div>
-            <div className="flex-row pt-8">
-              <a className="flex items-center justify-end text-h1 font-bold text-red">
-                我要贊助
-                <span className="material-icons" style={{ fontSize: "62px" }}>
-                  north_east
-                </span>
-              </a>
-              <a className="flex items-center justify-end text-h2 font-bold text-red">
-                索取贊助徵求書
-                <span className="material-icons" style={{ fontSize: "62px" }}>
-                  north_east
-                </span>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-span-2 grid grid-rows-2 gap-4">
-          <div className="grid grid-cols-3 gap-4">
-            <div className="relative col-span-2 rounded-[32px] border-2 border-blue bg-white p-6">
-              <div>
-                <h1 className="text-h2 font-semibold tracking-[2px] text-blue">
-                  自 2013 以來，我們帶領了無數學子進入資訊的殿堂，更做到：
-                </h1>
-                <p className="absolute bottom-6 left-6 text-justify text-normal leading-8 text-blue">
-                  {">"} 累積逾 11,000 名與會者
-                  <br />
-                  {">"} 超過 400 位學生講者
-                  <br />
-                  {">"} 舉辦逾 400 場聚會和講座、8 場夏令營
-                  <br />
-                  {">"} 扶植近 1,100 名熱愛資訊的志工
-                </p>
-              </div>
-            </div>
-            <div
-              className="col-span-1 rounded-[32px] border-2 border-blue bg-white bg-cover bg-center p-6"
-              style={{ backgroundImage: `url('/cfp/stats/1.jpg')` }}
-            ></div>
-          </div>
-          <div className="grid grid-cols-3 gap-4">
-            <div
-              className="col-span-1 rounded-[32px] border-2 border-blue bg-white bg-cover bg-center p-6"
-              style={{ backgroundImage: `url('/cfp/stats/2.jpg')` }}
-            ></div>
-            <div className="relative col-span-2 rounded-[32px] border-2 border-blue bg-white p-6">
-              <h1 className="text-h2 font-semibold tracking-[2px] text-blue">
-                此外，我們也樂見企業與學生的近距離交流，成為我們的合作夥伴您可以：
-              </h1>
-              <p className="absolute bottom-6 left-6 text-justify text-normal leading-8 text-blue">
-                {">"} 接觸學生資訊人才
-                <br />
-                {">"} 提升品牌影響力
-                <br />
-                {">"} 深入學生群體
-                <br />
-                {">"} 推廣產品內容
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <DesktopLayout />
 
       {/* Tablet Layout */}
-      <div className="relative grid grid-rows-2 gap-4 bg-light-brown bg-cover bg-[left_20%_center] px-10 max-sm:hidden min-[820px]:hidden lg:bg-center">
-        <div className="row-span-1 grid h-auto grid-cols-2 gap-4">
-          <div className="col-span-1 rounded-[32px] border-2 border-blue bg-white p-6">
-            <div>
-              <h1 className="text-h1 font-black tracking-[2px] text-blue">
-                支持我們
-              </h1>
-              <p className="text-justify text-normal leading-7 text-blue">
-                SITCON
-                秉持著不向與會者收費的原則，十年以來致力於推廣學生資訊教育，創造學生交流、教學互長的機會。
-                <br />
-                <br />
-                如果您也認同 SITCON 的理念，歡迎以行動支持 SITCON
-                的運作，點擊下方「個人贊助方案」，您的支持將會成為我們的動力，讓更多學生能在舞台上展現自我、讓資訊教育遍及臺灣的每個角落！
-                <br />
-                <br />
-                若您有興趣成為我們的贊助夥伴，歡迎點擊下方「索取贊助徵求書」，了解詳細合作方案，或是利用
-                contact@sitcon.org 聯絡我們，商討更多的合作方式！
-              </p>
-            </div>
-            <div className="flex-row pt-8">
-              <a className="flex items-center justify-end text-h1 font-bold text-red">
-                我要贊助
-                <span className="material-icons" style={{ fontSize: "62px" }}>
-                  north_east
-                </span>
-              </a>
-              <a className="flex items-center justify-end text-h2 font-bold text-red">
-                索取贊助徵求書
-                <span className="material-icons" style={{ fontSize: "62px" }}>
-                  north_east
-                </span>
-              </a>
-            </div>
-          </div>
-          <div className="col-span-1 grid grid-rows-2 gap-4">
-            <div className="relative col-span-2 rounded-[32px] border-2 border-blue bg-white p-6">
-              <div>
-                <h1 className="text-[26px] font-semibold tracking-[2px] text-blue">
-                  自 2013 以來，我們帶領了無數學子進入資訊的殿堂，更做到：
-                </h1>
-                <p className="absolute bottom-6 left-6 text-justify text-normal leading-8 text-blue">
-                  {">"} 累積逾 11,000 名與會者
-                  <br />
-                  {">"} 超過 400 位學生講者
-                  <br />
-                  {">"} 舉辦逾 400 場聚會和講座、8 場夏令營
-                  <br />
-                  {">"} 扶植近 1,100 名熱愛資訊的志工
-                </p>
-              </div>
-            </div>
-            <div className="relative col-span-2 rounded-[32px] border-2 border-blue bg-white p-6">
-              <h1 className="text-[26px] font-semibold tracking-[2px] text-blue">
-                此外，我們也樂見企業與學生的近距離交流，成為我們的合作夥伴您可以：
-              </h1>
-              <p className="absolute bottom-6 left-6 text-justify text-normal leading-8 text-blue">
-                {">"} 接觸學生資訊人才
-                <br />
-                {">"} 提升品牌影響力
-                <br />
-                {">"} 深入學生群體
-                <br />
-                {">"} 推廣產品內容
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="row-span-1 grid grid-cols-3 gap-4">
-          <div
-            className="col-span-2 h-[360px] rounded-[32px] border-2 border-blue bg-white bg-cover bg-center p-6"
-            style={{ backgroundImage: `url('/cfp/stats/2.jpg')` }}
-          ></div>
-          <div
-            className="col-span-1 h-[360px] rounded-[32px] border-2 border-blue bg-white bg-cover bg-center p-6"
-            style={{ backgroundImage: `url('/cfp/stats/1.jpg')` }}
-          ></div>
-        </div>
-      </div>
+      <TabletLayout />
 
       {/* Phone Layout */}
-      <div className="relative flex flex-col gap-4 bg-blue bg-cover bg-[left_20%_center] px-10 pb-6 sm:hidden lg:bg-center">
-        <h1 className="text-center text-h1-mobile font-semibold text-black">
-          支持我們
+      <PhoneLayout />
+    </>
+  );
+}
+
+function CloseTouchSection() {
+  return (
+    <div className="bg-gray p-8 sm:relative sm:col-span-2 sm:rounded-[32px] sm:border-2 sm:border-blue sm:bg-white sm:p-6">
+      <h1 className="text-[26px] font-semibold tracking-[2px] text-black sm:text-blue min-[820px]:text-h2">
+        此外，我們也樂見企業與學生的近距離交流，成為我們的合作夥伴您可以：
+      </h1>
+      <p className="pt-5 text-justify text-normal-mobile font-semibold leading-8 text-black sm:absolute sm:bottom-6 sm:left-6 sm:pt-0 sm:text-normal sm:font-normal sm:text-blue">
+        {">"} 接觸學生資訊人才
+        <br />
+        {">"} 提升品牌影響力
+        <br />
+        {">"} 深入學生群體
+        <br />
+        {">"} 推廣產品內容
+      </p>
+    </div>
+  );
+}
+
+function From2013() {
+  return (
+    <>
+      <div className="bg-gray p-8 sm:hidden">
+        <h1 className="text-[26px] font-semibold tracking-[2px] text-black">
+          自 2013 以來，我們帶領了無數學子進入資訊的殿堂，更做到：
         </h1>
-        <div className="bg-gray p-8">
-          <h1 className="text-[26px] font-semibold tracking-[2px] text-black">
-            自 2013 以來，我們帶領了無數學子進入資訊的殿堂，更做到：
-          </h1>
-          <p className="pt-5 text-justify text-normal-mobile font-semibold leading-8 text-black">
-            {">"} 累積逾 11,000 名與會者
-            <br />
-            {">"} 超過 400 位學生講者
-            <br />
-            {">"} 舉辦逾 400 場聚會和講座、8 場夏令營
-            <br />
-            {">"} 扶植近 1,100 名熱愛資訊的志工
-          </p>
+        <p className="pt-5 text-justify text-normal-mobile font-semibold leading-8 text-black">
+          {">"} 累積逾 11,000 名與會者
+          <br />
+          {">"} 超過 400 位學生講者
+          <br />
+          {">"} 舉辦逾 400 場聚會和講座、8 場夏令營
+          <br />
+          {">"} 扶植近 1,100 名熱愛資訊的志工
+        </p>
+      </div>
+
+      <div className="relative col-span-2 rounded-[32px] border-2 border-blue bg-white p-6 max-[820px]:hidden">
+        <h1 className="text-h2 font-semibold tracking-[2px] text-blue">
+          自 2013 以來，我們帶領了無數學子進入資訊的殿堂，更做到：
+        </h1>
+        <p className="absolute bottom-6 left-6 text-justify text-normal leading-8 text-blue">
+          {">"} 累積逾 11,000 名與會者
+          <br />
+          {">"} 超過 400 位學生講者
+          <br />
+          {">"} 舉辦逾 400 場聚會和講座、8 場夏令營
+          <br />
+          {">"} 扶植近 1,100 名熱愛資訊的志工
+        </p>
+      </div>
+
+      <div className="relative col-span-2 rounded-[32px] border-2 border-blue bg-white p-6 max-sm:hidden min-[820px]:hidden">
+        <h1 className="text-[26px] font-semibold tracking-[2px] text-blue">
+          自 2013 以來，我們帶領了無數學子進入資訊的殿堂，更做到：
+        </h1>
+        <p className="absolute bottom-6 left-6 text-justify text-normal leading-8 text-blue">
+          {">"} 累積逾 11,000 名與會者
+          <br />
+          {">"} 超過 400 位學生講者
+          <br />
+          {">"} 舉辦逾 400 場聚會和講座、8 場夏令營
+          <br />
+          {">"} 扶植近 1,100 名熱愛資訊的志工
+        </p>
+      </div>
+    </>
+  );
+}
+
+function PhoneLayout() {
+  return (
+    <div className="relative flex flex-col gap-4 bg-blue bg-cover bg-[left_20%_center] px-10 pb-6 sm:hidden lg:bg-center">
+      <h1 className="text-center text-h1-mobile font-semibold text-black">
+        支持我們
+      </h1>
+      <From2013 />
+      <CloseTouchSection />
+      <div className="bg-gray p-8">
+        <p className="text-justify text-normal-mobile font-normal leading-6 text-black">
+          SITCON
+          秉持著不向與會者收費的原則，十年以來致力於推廣學生資訊教育，創造學生交流、教學互長的機會。
+          <br />
+          <br />
+          如果您也認同 SITCON 的理念，歡迎以行動支持 SITCON
+          的運作，點擊下方「個人贊助方案」，您的支持將會成為我們的動力，讓更多學生能在舞台上展現自我、讓資訊教育遍及臺灣的每個角落！
+          <br />
+          <br />
+          若您有興趣成為我們的贊助夥伴，歡迎點擊下方「索取贊助徵求書」，了解詳細合作方案，或是利用
+          contact@sitcon.org 聯絡我們，商討更多的合作方式！
+        </p>
+        <div className="flex-row pt-8">
+          <a className="flex items-center justify-end text-[40px] font-bold text-black">
+            我要贊助
+            <span className="material-icons" style={{ fontSize: "44px" }}>
+              north_east
+            </span>
+          </a>
+          <a className="flex items-center justify-end text-[32px] font-bold text-black">
+            索取贊助徵求書
+            <span className="material-icons" style={{ fontSize: "44px" }}>
+              north_east
+            </span>
+          </a>
         </div>
-        <div className="bg-gray p-8">
-          <h1 className="text-[26px] font-semibold tracking-[2px] text-black">
-            此外，我們也樂見企業與學生的近距離交流，成為我們的合作夥伴您可以：
+      </div>
+    </div>
+  );
+}
+
+function TabletLayout() {
+  return (
+    <div className="relative grid grid-rows-2 gap-4 bg-light-brown bg-cover bg-[left_20%_center] px-10 max-sm:hidden min-[820px]:hidden lg:bg-center">
+      <div className="row-span-1 grid h-auto grid-cols-2 gap-4">
+        <div className="col-span-1 rounded-[32px] border-2 border-blue bg-white p-6">
+          <h1 className="text-h1 font-black tracking-[2px] text-blue">
+            支持我們
           </h1>
-          <p className="pt-5 text-justify text-normal-mobile font-semibold leading-8 text-black">
-            {">"} 接觸學生資訊人才
-            <br />
-            {">"} 提升品牌影響力
-            <br />
-            {">"} 深入學生群體
-            <br />
-            {">"} 推廣產品內容
-          </p>
-        </div>
-        <div className="bg-gray p-8">
-          <p className="text-justify text-normal-mobile font-normal leading-6 text-black">
+          <p className="text-justify text-normal leading-7 text-blue">
             SITCON
             秉持著不向與會者收費的原則，十年以來致力於推廣學生資訊教育，創造學生交流、教學互長的機會。
             <br />
@@ -381,21 +313,91 @@ export default function Home() {
             contact@sitcon.org 聯絡我們，商討更多的合作方式！
           </p>
           <div className="flex-row pt-8">
-            <a className="flex items-center justify-end text-[40px] font-bold text-black">
+            <a className="flex items-center justify-end text-h1 font-bold text-red">
               我要贊助
-              <span className="material-icons" style={{ fontSize: "44px" }}>
+              <span className="material-icons" style={{ fontSize: "62px" }}>
                 north_east
               </span>
             </a>
-            <a className="flex items-center justify-end text-[32px] font-bold text-black">
+            <a className="flex items-center justify-end text-h2 font-bold text-red">
               索取贊助徵求書
-              <span className="material-icons" style={{ fontSize: "44px" }}>
+              <span className="material-icons" style={{ fontSize: "62px" }}>
+                north_east
+              </span>
+            </a>
+          </div>
+        </div>
+        <div className="col-span-1 grid grid-rows-2 gap-4">
+          <From2013 />
+          <CloseTouchSection />
+        </div>
+      </div>
+      <div className="row-span-1 grid grid-cols-3 gap-4">
+        <div
+          className="col-span-2 h-[360px] rounded-[32px] border-2 border-blue bg-white bg-cover bg-center p-6"
+          style={{ backgroundImage: `url('/cfp/stats/2.jpg')` }}
+        ></div>
+        <div
+          className="col-span-1 h-[360px] rounded-[32px] border-2 border-blue bg-white bg-cover bg-center p-6"
+          style={{ backgroundImage: `url('/cfp/stats/1.jpg')` }}
+        ></div>
+      </div>
+    </div>
+  );
+}
+
+function DesktopLayout() {
+  return (
+    <div className="relative grid grid-cols-3 gap-4 bg-light-brown bg-cover bg-[left_20%_center] px-10 max-[820px]:hidden lg:bg-center">
+      <div className="col-span-1 h-auto">
+        <div className="rounded-[32px] border-2 border-blue bg-white p-6">
+          <h1 className="text-h1 font-black tracking-[2px] text-blue">
+            支持我們
+          </h1>
+          <p className="text-justify text-normal leading-7 text-blue">
+            SITCON
+            秉持著不向與會者收費的原則，十年以來致力於推廣學生資訊教育，創造學生交流、教學互長的機會。
+            <br />
+            <br />
+            如果您也認同 SITCON 的理念，歡迎以行動支持 SITCON
+            的運作，點擊下方「個人贊助方案」，您的支持將會成為我們的動力，讓更多學生能在舞台上展現自我、讓資訊教育遍及臺灣的每個角落！
+            <br />
+            <br />
+            若您有興趣成為我們的贊助夥伴，歡迎點擊下方「索取贊助徵求書」，了解詳細合作方案，或是利用
+            contact@sitcon.org 聯絡我們，商討更多的合作方式！
+          </p>
+          <div className="flex-row pt-8">
+            <a className="flex items-center justify-end text-h1 font-bold text-red">
+              我要贊助
+              <span className="material-icons" style={{ fontSize: "62px" }}>
+                north_east
+              </span>
+            </a>
+            <a className="flex items-center justify-end text-h2 font-bold text-red">
+              索取贊助徵求書
+              <span className="material-icons" style={{ fontSize: "62px" }}>
                 north_east
               </span>
             </a>
           </div>
         </div>
       </div>
-    </>
+      <div className="col-span-2 grid grid-rows-2 gap-4">
+        <div className="grid grid-cols-3 gap-4">
+          <From2013 />
+          <div
+            className="col-span-1 rounded-[32px] border-2 border-blue bg-white bg-cover bg-center p-6"
+            style={{ backgroundImage: `url('/cfp/stats/1.jpg')` }}
+          ></div>
+        </div>
+        <div className="grid grid-cols-3 gap-4">
+          <div
+            className="col-span-1 rounded-[32px] border-2 border-blue bg-white bg-cover bg-center p-6"
+            style={{ backgroundImage: `url('/cfp/stats/2.jpg')` }}
+          ></div>
+          <CloseTouchSection />
+        </div>
+      </div>
+    </div>
   );
 }
