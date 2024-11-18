ba054f9a9ea89c607c4f13bc229ae9b29893289b
Dada878 <xx0932399@gmail.com>
2024-11-19 07:03:28 +0800

    feat: add horizontal padding to layout

--- app/(cfp)/(submission)/layout.tsx
+++ app/(cfp)/(submission)/layout.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="bg-blue text-light-brown">
-      <div className="mx-auto max-w-[345px] mobile:max-w-[645px] tablet:max-w-[840px] desktop:max-w-[1120px]">
+    <div className="flex justify-center bg-blue text-light-brown">
+      <div className="mx-10 max-w-[345px] mobile:max-w-[645px] tablet:max-w-[840px] desktop:max-w-[1120px]">
         {children}
       </div>
     </div>
