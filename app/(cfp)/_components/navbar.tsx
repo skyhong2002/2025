d31deba5b71d105ec76316024333c6250c0bdb29
Dada878 <xx0932399@gmail.com>
2024-11-18 20:28:57 +0800

    style: improve formatting and adjust component class order

--- app/(cfp)/_components/navbar.tsx
+++ app/(cfp)/_components/navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue sticky left-0 right-0 top-0 z-10 flex justify-center">
+    <nav className="sticky left-0 right-0 top-0 z-10 flex justify-center bg-blue">
       <ul className="container flex items-center justify-between p-4">
         <Image
           src="/assets/logo.png"
@@ -12,7 +12,7 @@ export default function Navbar() {
           width={130}
           height={65}
         />
-        <ul className="text-light-brown flex items-center justify-end gap-8">
+        <ul className="flex items-center justify-end gap-8 text-light-brown">
           <NavbarItem link="/">首頁</NavbarItem>
           <NavbarItem link="/theme">年會主題</NavbarItem>
           <NavbarItem link="/normal">一般議程徵稿</NavbarItem>
