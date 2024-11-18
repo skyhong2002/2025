bb72664e3e5e38aa8730b8db8228602f33fb926d
Dada878 <xx0932399@gmail.com>
2024-11-19 05:09:02 +0800

    feat: improve home page with responsive max-width settings and reorder classes

--- app/(cfp)/page.tsx
+++ app/(cfp)/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
   return (
     <>
       <div
-        className="relative h-[calc(100svh-64px)] min-h-[600px] bg-white bg-cover bg-[left_20%_center] max-[820px]:bg-gray lg:bg-center"
+        className="lg:bg-center relative h-[calc(100svh-64px)] min-h-[600px] bg-white bg-cover bg-[left_20%_center] max-[820px]:bg-gray"
         style={{
           backgroundImage: `url('')`,
         }}
@@ -61,11 +61,11 @@ export default function Home() {
         <div className="absolute bottom-14 flex w-full flex-col gap-4">
           <div className="flex flex-col items-center justify-center">
             <div>
-              <p className="text-normal max-[820px]:text-h3 max-sm:text-[22px]">
+              <p className="max-sm:text-[22px] text-normal max-[820px]:text-h3">
                 距離海報投稿截止還有
               </p>
             </div>
-            <div className="flex flex-row max-sm:w-[80%] max-sm:justify-center max-sm:rounded-[208px] max-sm:border-2 max-sm:border-black max-sm:p-3">
+            <div className="max-sm:w-[80%] max-sm:justify-center max-sm:rounded-[208px] max-sm:border-2 max-sm:border-black max-sm:p-3 flex flex-row">
               {Object.entries(timeLeft).map(([key, value]) => (
                 <div key={key}>
                   <div className="flex flex-row items-center justify-center">
@@ -92,7 +92,7 @@ export default function Home() {
                           filter: "blur(0.1em)",
                           transformOrigin: "bottom",
                         }}
-                        className="text-5xl max-[820px]:text-6xl max-sm:text-5xl"
+                        className="max-sm:text-5xl text-5xl max-[820px]:text-6xl"
                       >
                         {formatToTwoDigits(value)}
                       </motion.span>
@@ -109,75 +109,78 @@ export default function Home() {
               ))}
             </div>
           </div>
-          <div className="flex flex-row justify-center gap-5 max-sm:grid max-sm:grid-cols-2 max-sm:px-4">
+          <div className="max-sm:grid max-sm:grid-cols-2 max-sm:px-4 flex flex-row justify-center gap-5">
             <Link
               href="#"
-              className="w-[140px] rounded-[50px] bg-[#FFFFFF] py-1 text-center max-[820px]:text-[18px] max-[820px]:font-semibold max-sm:w-full max-sm:py-2 max-sm:text-h2-mobile"
+              className="max-sm:w-full max-sm:py-2 max-sm:text-h2-mobile w-[140px] rounded-[50px] bg-[#FFFFFF] py-1 text-center max-[820px]:text-[18px] max-[820px]:font-semibold"
             >
               前往投稿
             </Link>
             <Link
               href="#"
-              className="w-[140px] rounded-[50px] bg-[#FFFFFF] py-1 text-center max-[820px]:text-[18px] max-sm:w-full max-sm:py-2 max-sm:text-h2-mobile"
+              className="max-sm:w-full max-sm:py-2 max-sm:text-h2-mobile w-[140px] rounded-[50px] bg-[#FFFFFF] py-1 text-center max-[820px]:text-[18px]"
             >
               加入行事曆
             </Link>
           </div>
         </div>
       </div>
-      <div className="relative grid grid-cols-2 bg-light-brown bg-cover bg-[left_20%_center] px-10 max-[820px]:px-6 max-sm:grid-cols-none max-sm:bg-blue max-sm:text-center lg:bg-center">
-        <div className="flex">
-          <div className="gap-24 py-32">
-            <div>
-              <h1 className="text-h1 font-black tracking-[2px] text-blue max-[820px]:text-black max-sm:text-h1-mobile max-sm:font-semibold">
-                關於 SITCON
-              </h1>
-              <p className="text-justify text-normal leading-7 text-blue max-sm:leading-9 max-sm:text-black">
-                學生計算機年會（Students’ Information Technology Conference）自
-                2013
-                年發起，以學生為本、由學生自發舉辦，長期投身學生資訊教育與推廣開源精神，希望引領更多學子踏入資訊的殿堂，更冀望所有對資訊有興趣的學生，能夠在年會裏齊聚一堂，彼此激盪、傳承、啟發，達到「學以致用、教學相長」的實際展現。
-              </p>
-            </div>
-            <div>
-              <h1 className="pt-16 text-h1 font-black tracking-[2px] text-blue max-[820px]:text-black max-sm:text-h1-mobile max-sm:font-semibold">
-                參與討論
-              </h1>
-              <p className="text-justify text-normal leading-7 text-blue max-sm:leading-9 max-sm:text-black">
-                SITCON
-                是個開放的社群，許多籌備的相關事項都會在公開的郵件論壇上進行討論，如果有興趣參與，歡迎訂閱
-                SITCON 的郵件論壇並貢獻你的想法！
-                <br />
-                <br /> 若你想進一步了解過往討論的內容，也歡迎到 GitLab 上查看
-                SITCON 2024 的籌備歷程。
-              </p>
-            </div>
-            <div className="flex gap-4 pt-14 max-[820px]:grid max-[820px]:grid-cols-3 max-[820px]:gap-5 max-sm:grid-cols-2">
-              <Link
-                href="https://groups.google.com/g/sitcon-general/"
-                className="w-[140px] rounded-[50px] bg-gray py-3 text-center font-bold max-[820px]:col-span-2 max-[820px]:w-auto max-[820px]:rounded-lg max-[820px]:text-[18px] max-[712px]:my-auto max-[712px]:text-[14px] max-sm:col-span-1 max-sm:rounded-[72px] max-sm:text-[18px] max-sm:font-normal"
-              >
-                訂閱郵件論壇
-              </Link>
-              <Link
-                href="https://gitlab.com/sitcon-tw/2024/board"
-                className="w-[140px] rounded-[50px] bg-gray py-3 text-center font-bold max-[820px]:col-span-1 max-[820px]:w-auto max-[820px]:rounded-lg max-[820px]:text-[18px] max-[712px]:my-auto max-[712px]:text-[14px] max-sm:rounded-[72px] max-sm:text-[18px] max-sm:font-normal"
-              >
-                查看 GitLab
-              </Link>
+
+      <div className="flex flex-col items-center justify-center">
+        <div className="max-sm:grid-cols-none max-sm:bg-blue max-sm:text-center lg:bg-center relative grid max-w-[1120px] grid-cols-2 bg-light-brown bg-cover bg-[left_20%_center] px-10 max-[820px]:px-6">
+          <div className="flex">
+            <div className="gap-24 py-32">
+              <div>
+                <h1 className="max-sm:text-h1-mobile max-sm:font-semibold text-h1 font-black tracking-[2px] text-blue max-[820px]:text-black">
+                  關於 SITCON
+                </h1>
+                <p className="max-sm:leading-9 max-sm:text-black text-justify text-normal leading-7 text-blue">
+                  學生計算機年會（Students’ Information Technology
+                  Conference）自 2013
+                  年發起，以學生為本、由學生自發舉辦，長期投身學生資訊教育與推廣開源精神，希望引領更多學子踏入資訊的殿堂，更冀望所有對資訊有興趣的學生，能夠在年會裏齊聚一堂，彼此激盪、傳承、啟發，達到「學以致用、教學相長」的實際展現。
+                </p>
+              </div>
+              <div>
+                <h1 className="max-sm:text-h1-mobile max-sm:font-semibold pt-16 text-h1 font-black tracking-[2px] text-blue max-[820px]:text-black">
+                  參與討論
+                </h1>
+                <p className="max-sm:leading-9 max-sm:text-black text-justify text-normal leading-7 text-blue">
+                  SITCON
+                  是個開放的社群，許多籌備的相關事項都會在公開的郵件論壇上進行討論，如果有興趣參與，歡迎訂閱
+                  SITCON 的郵件論壇並貢獻你的想法！
+                  <br />
+                  <br /> 若你想進一步了解過往討論的內容，也歡迎到 GitLab 上查看
+                  SITCON 2024 的籌備歷程。
+                </p>
+              </div>
+              <div className="max-sm:grid-cols-2 flex gap-4 pt-14 max-[820px]:grid max-[820px]:grid-cols-3 max-[820px]:gap-5">
+                <Link
+                  href="https://groups.google.com/g/sitcon-general/"
+                  className="max-sm:col-span-1 max-sm:rounded-[72px] max-sm:text-[18px] max-sm:font-normal w-[140px] rounded-[50px] bg-gray py-3 text-center font-bold max-[820px]:col-span-2 max-[820px]:w-auto max-[820px]:rounded-lg max-[820px]:text-[18px] max-[712px]:my-auto max-[712px]:text-[14px]"
+                >
+                  訂閱郵件論壇
+                </Link>
+                <Link
+                  href="https://gitlab.com/sitcon-tw/2024/board"
+                  className="max-sm:rounded-[72px] max-sm:text-[18px] max-sm:font-normal w-[140px] rounded-[50px] bg-gray py-3 text-center font-bold max-[820px]:col-span-1 max-[820px]:w-auto max-[820px]:rounded-lg max-[820px]:text-[18px] max-[712px]:my-auto max-[712px]:text-[14px]"
+                >
+                  查看 GitLab
+                </Link>
+              </div>
             </div>
           </div>
+          <div className="flex"></div>
         </div>
-        <div className="flex"></div>
+
+        {/* Desktop Layout */}
+        <DesktopLayout />
+
+        {/* Tablet Layout */}
+        <TabletLayout />
+
+        {/* Phone Layout */}
+        <PhoneLayout />
       </div>
-
-      {/* Desktop Layout */}
-      <DesktopLayout />
-
-      {/* Tablet Layout */}
-      <TabletLayout />
-
-      {/* Phone Layout */}
-      <PhoneLayout />
 
       <div className="pb-12"></div>
     </>
@@ -186,11 +189,11 @@ export default function Home() {
 
 function CloseTouchSection() {
   return (
-    <div className="bg-gray p-8 sm:relative sm:col-span-2 sm:rounded-[32px] sm:border-2 sm:border-blue sm:bg-white sm:p-6">
-      <h1 className="text-[26px] font-semibold tracking-[2px] text-black sm:text-blue min-[820px]:text-h2">
+    <div className="sm:relative sm:col-span-2 sm:rounded-[32px] sm:border-2 sm:border-blue sm:bg-white sm:p-6 bg-gray p-8">
+      <h1 className="sm:text-blue text-[26px] font-semibold tracking-[2px] text-black min-[820px]:text-h2">
         此外，我們也樂見企業與學生的近距離交流，成為我們的合作夥伴您可以：
       </h1>
-      <p className="pt-5 text-justify text-normal-mobile font-semibold leading-8 text-black sm:absolute sm:bottom-6 sm:left-6 sm:pt-0 sm:text-normal sm:font-normal sm:text-blue">
+      <p className="sm:absolute sm:bottom-6 sm:left-6 sm:pt-0 sm:text-normal sm:font-normal sm:text-blue pt-5 text-justify text-normal-mobile font-semibold leading-8 text-black">
         {">"} 接觸學生資訊人才
         <br />
         {">"} 提升品牌影響力
@@ -206,7 +209,7 @@ function CloseTouchSection() {
 function From2013() {
   return (
     <>
-      <div className="bg-gray p-8 sm:hidden">
+      <div className="sm:hidden bg-gray p-8">
         <h1 className="text-[26px] font-semibold tracking-[2px] text-black">
           自 2013 以來，我們帶領了無數學子進入資訊的殿堂，更做到：
         </h1>
@@ -236,7 +239,7 @@ function From2013() {
         </p>
       </div>
 
-      <div className="relative col-span-2 rounded-[32px] border-2 border-blue bg-white p-6 max-sm:hidden min-[820px]:hidden">
+      <div className="max-sm:hidden relative col-span-2 rounded-[32px] border-2 border-blue bg-white p-6 min-[820px]:hidden">
         <h1 className="text-[26px] font-semibold tracking-[2px] text-blue">
           自 2013 以來，我們帶領了無數學子進入資訊的殿堂，更做到：
         </h1>
@@ -256,7 +259,7 @@ function From2013() {
 
 function PhoneLayout() {
   return (
-    <div className="relative flex flex-col gap-4 bg-blue bg-cover bg-[left_20%_center] px-10 pb-6 sm:hidden lg:bg-center">
+    <div className="sm:hidden lg:bg-center relative flex flex-col gap-4 bg-blue bg-cover bg-[left_20%_center] px-10 pb-6">
       <h1 className="text-center text-h1-mobile font-semibold text-black">
         支持我們
       </h1>
@@ -296,7 +299,7 @@ function PhoneLayout() {
 
 function TabletLayout() {
   return (
-    <div className="relative grid grid-rows-2 gap-4 bg-light-brown bg-cover bg-[left_20%_center] px-10 max-sm:hidden min-[820px]:hidden lg:bg-center">
+    <div className="max-sm:hidden lg:bg-center relative grid max-w-[1120px] grid-rows-2 gap-4 bg-light-brown bg-cover bg-[left_20%_center] px-10 min-[820px]:hidden">
       <div className="row-span-1 grid h-auto grid-cols-2 gap-4">
         <div className="col-span-1 rounded-[32px] border-2 border-blue bg-white p-6">
           <h1 className="text-h1 font-black tracking-[2px] text-blue">
@@ -350,7 +353,7 @@ function TabletLayout() {
 
 function DesktopLayout() {
   return (
-    <div className="relative grid grid-cols-3 gap-4 bg-light-brown bg-cover bg-[left_20%_center] px-10 max-[820px]:hidden lg:bg-center">
+    <div className="lg:bg-center relative grid max-w-[1120px] grid-cols-3 gap-4 bg-light-brown bg-cover bg-[left_20%_center] px-10 max-[820px]:hidden">
       <div className="col-span-1 h-auto">
         <div className="rounded-[32px] border-2 border-blue bg-white p-6">
           <h1 className="text-h1 font-black tracking-[2px] text-blue">
