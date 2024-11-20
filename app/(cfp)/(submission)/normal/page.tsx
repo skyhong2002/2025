5f603038d9b8a878c36733ee2e16981432215e3e
Pizza6inch <pizza6inch@gmail.com>
2024-11-20 20:00:33 +0800

    normal page

--- app/(cfp)/(submission)/normal/page.tsx
+++ app/(cfp)/(submission)/normal/page.tsx
@@ -20,6 +20,7 @@ export default function Page() {
     { id: "rehearsal", title: "彩排" },
     { id: "qa", title: "Q & A" },
   ];
+
   return (
     <div className="py-8 text-normal-mobile md:text-normal">
       <h1 className="mb-4 text-h1-mobile font-extrabold md:text-h1">
@@ -70,7 +71,7 @@ export default function Page() {
           投稿主題範例
         </h2>
         <p className="leading-10">
-          <span className="text-lime-400">SITCON </span>
+          <span className="font-bold text-lime-400">SITCON </span>
           作為學生展現自己的舞台，我們特別期待以學生為主體的稿件，例如：從學生角度出發的經驗分享、技術分享，專題研究成果、獨立研究甘苦談，或探討學生相關議題等等。
           不論是以技術探討形式，或輕鬆的分享方式，任何與資訊科技、電腦技術相關的講題，我們都非常歡迎投稿！
           除此之外，您也可以透過參與投稿者小聚或前往 SITCON
@@ -80,12 +81,15 @@ export default function Page() {
           PRESENTATION
         </h3>
         <p className="leading-10">
-          Presentation 包含完整 30 分鐘的議程與 10 分鐘的問答時間，共 40
+          Presentation 包含完整
+          <span className="hightlight"> 30 </span>分鐘的議程與
+          <span className="hightlight"> 10 </span>
+          分鐘的問答時間，共 <span className="hightlight"> 40 </span>
           分鐘，讓您有充分的時間展示想法、描述經歷、和與會者交流。
         </p>
         <h3 className="my-4 text-h3-mobile font-bold md:text-h3">ESPRESSO</h3>
         <p className="leading-10">
-          Espresso 僅有 10
+          Espresso 僅有 <span className="hightlight"> 10 </span>
           分鐘，挑戰最為濃縮與精華的議程，為與會者帶來短而精緻的知識洗禮。
         </p>
       </section>
@@ -98,67 +102,93 @@ export default function Page() {
         </h3>
         <ul className="list-inside list-disc leading-10">
           <li>
-            投稿類型
-            Type：必填、公開。請寫明所欲投稿為何種議程類型（Presentation、Espresso）。
+            <span className="subTitle">投稿類型 Type：</span>
+            <span className="hightlight">必填、公開</span>
+            。請寫明所欲投稿為何種議程類型（Presentation、Espresso）。
           </li>
           <li>
-            題目 Title：必填、公開。請不要超出 20 字，題目無須與年會主題相關。
+            <span className="subTitle">題目 Title：</span>
+            <span className="hightlight">必填、公開</span>
+            。請不要超出<span className="hightlight"> 20 </span>
+            字，題目無須與年會主題相關。
           </li>
           <li>
-            摘要 Abstract：必填、公開。建議 150–250 字，使用於宣傳與網站公告。
+            <span className="subTitle">摘要 Abstract：</span>
+            <span className="hightlight">必填、公開</span>。建議
+            <span className="hightlight"> 150~250 </span>
+            字，使用於宣傳與網站公告。
           </li>
           <li>
-            演講大綱
-            Outline：必填。請說明議程的內容大綱與時間分配。若為多位講者共同分享，請說明各個講者如何分配演講內容。
+            <span className="subTitle">演講大綱 Outline：</span>
+            <span className="hightlight">必填</span>
+            。請說明議程的內容大綱與時間分配。若為多位講者共同分享，請說明各個講者如何分配演講內容。
           </li>
-          <li>
-            目標受眾 Target
-            Audience：必填。請說明您期待該議程的目標受眾為怎麼樣的人。例如：「嚮往成為前端開發者的初學者」、「欲精進某技能的後端工程師」等。
+          <li className="pl-[22px] -indent-[22px]">
+            <span className="subTitle">目標受眾 Target Audience：</span>
+            <span className="hightlight">必填</span>
+            。請說明您期待該議程的目標受眾為怎麼樣的人。例如：「嚮往成為前端開發者的初學者」、「欲精進某技能的後端工程師」等。在這邊為您提供過往的會眾比例：大學生
+            58%、高中/職生 22%、社會人士 8%、研究生 11%、國中生 1%
           </li>
-          <p>
-            在這邊為您提供過往的會眾比例：大學生 58%、高中/職生 22%、社會人士
-            8%、研究生 11%、國中生 1%
-          </p>
-          <li>
-            先備知識 Prior
-            Knowledge：必填、公開。請說明該議程的與會者所需具備的先備知識。例如：「能理解
-            Python 基礎語法」、「能熟練運用 Class 與 OOP」等。
+          <p></p>
+          <li className="pl-[22px] -indent-[22px]">
+            <span className="subTitle">先備知識 Prior Knowledge：</span>
+            <span className="hightlight">必填、公開</span>
+            。請說明該議程的與會者所需具備的
+            <span className="hightlight">先備知識</span>。例如：「能理解 Python
+            基礎語法」、「能熟練運用 Class 與 OOP」等。
           </li>
-          <li>
-            主題標籤 Labels：必填、公開。請填寫 1~4
-            個該議程相關的領域與知識標籤，例如：ML、Data
+          <li className="pl-[22px] -indent-[22px]">
+            <span className="subTitle">主題標籤 Labels：</span>
+            <span className="hightlight">必填、公開</span>
+            。請填寫 1~4 個該議程相關的
+            <span className="hightlight">領域與知識標籤</span>，例如：ML、Data
             Science、資安紅隊、blockchain、社團等；錄取後會與講者溝通微調後公開於官網議程表。
           </li>
-          <li>
-            詳細說明
-            Description：必填。不限字數，請盡可能詳細說明這個議程的相關細節，如議程架構、想傳達的內容及您認為可能可以說服審稿委員選擇您的稿件之資訊等，以利審稿委員更瞭解您的議程及想法。
+          <li className="pl-[22px] -indent-[22px]">
+            <span className="subTitle">詳細說明 Description：</span>
+            <span className="hightlight">必填</span>
+            。不限字數，請盡可能<span className="hightlight">詳細說明</span>
+            這個議程的相關細節，如議程架構、想傳達的內容及您認為可能可以說服審稿委員選擇您的稿件之資訊等，以利審稿委員更瞭解您的議程及想法。
           </li>
           <li>
-            附件
-            Attachment：選填。可以附上程式碼、投影片草稿或任何您認為有助於我們瞭解您議程的附件，請再三確認是否有開啟共用權限。
+            <span className="subTitle">附件 Attachment：</span>
+            選填。可以附上程式碼、投影片草稿或任何您認為有助於我們瞭解您議程的附件，請再三確認是否有開啟共用權限。
           </li>
         </ul>
         <h3 className="my-4 text-h3-mobile text-light-blue md:text-h3">
           個人資料
         </h3>
-        <p>
+        <p className="mb-2">
           除了暱稱 /
           名字、自我介紹被用於宣傳，其餘資訊僅需提供一人作為代表（當稿件同時有多位講者時），供議程組於聯絡時使用。
         </p>
         <ul className="list-inside list-disc leading-10">
           <li>
-            電子郵件
-            Email：必填。能讓大會聯絡的電子信箱。（注意：請您在大會的聯絡過程中，維持使用相同信箱）
+            <span className="subTitle">電子郵件 Email：</span>
+            <span className="hightlight">必填</span>
+            。能讓大會聯絡的電子信箱。（注意：請您在大會的聯絡過程中，維持使用相同信箱）
           </li>
-          <li>暱稱 / 名字 Name：必填、公開。公開於講者海報及網站的稱呼。</li>
           <li>
-            自我介紹 Self
-            Introduction：必填、公開。公開於講者海報及網站，將有助於會眾更加了解有關你的資訊。
+            <span className="subTitle">暱稱 / 名字 Name：</span>
+            <span className="hightlight">必填、公開</span>
+            。公開於講者海報及網站的稱呼。
           </li>
-          <li>手機號碼 Phone：必填。用於大會聯絡、確認資訊用的電話號碼。</li>
           <li>
-            其他聯絡方式 Other Contact
-            Information：必填。其他任何我們能聯絡您的方式（如 Telegram
+            <span className="subTitle">自我介紹 Self Introduction：</span>
+            <span className="hightlight">必填、公開</span>
+            。公開於講者海報及網站，將有助於會眾更加了解有關你的資訊。
+          </li>
+          <li>
+            <span className="subTitle">手機號碼 Phone：</span>
+            <span className="hightlight">必填</span>
+            。用於大會聯絡、確認資訊用的電話號碼。
+          </li>
+          <li className="pl-[22px] -indent-[22px]">
+            <span className="subTitle">
+              其他聯絡方式 Other Contact Information：
+            </span>
+            <span className="hightlight">必填</span>
+            。其他任何我們能聯絡您的方式（如 Telegram
             ID、住處電話等），以利我們在審稿時即時向您釐清投稿資訊。
           </li>
         </ul>
