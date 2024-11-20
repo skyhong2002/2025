06d218fd594d1b6720089550157b95103d93a730
Dada878 <xx0932399@gmail.com>
2024-11-20 21:07:37 +0800

    refactor: migrate Q&A functionality to a new FAQ component

--- app/cfp/_components/faq.tsx
+++ app/cfp/_components/faq.tsx
@@ -60,7 +60,7 @@ export default function FAQ({ QA }: { QA: QA[] }) {
                 animate={{
                   opacity: 1,
                   // y: 0,
-                  maxHeight: "400px",
+                  maxHeight: "100px",
                   paddingTop: "1rem",
                   paddingBottom: "1rem",
                   borderBottomRightRadius: "1rem",
