31f177e151c3e13294f03a3dd1ef52a6d09ce865
Dada878 <xx0932399@gmail.com>
2024-11-21 19:59:51 +0800

    feat: update text color to use text-primary utility for improved consistency

--- app/cfp/(submission)/normal/page.tsx
+++ app/cfp/(submission)/normal/page.tsx
@@ -52,7 +52,7 @@ export default function Page() {
         id="important-dates"
         className="my-4 -mt-[60px] space-y-4 pt-[80px] md:mb-16"
       >
-        <h2 className="text-h2-mobile font-extrabold text-light-blue md:text-h2">
+        <h2 className="text-h2-mobile font-extrabold text-primary md:text-h2">
           重要時程
         </h2>
         <Timeline />
@@ -62,11 +62,11 @@ export default function Page() {
         id="session-types"
         className="my-4 -mt-[60px] space-y-4 pt-[80px] md:mb-16"
       >
-        <h2 className="text-h2-mobile font-extrabold text-light-blue md:text-h2">
+        <h2 className="text-h2-mobile font-extrabold text-primary md:text-h2">
           議程種類
         </h2>
         <div className="mb-6 mt-4 grid grid-rows-2 gap-3 lg:grid-cols-2 lg:grid-rows-1">
-          <div className="flex flex-col items-center rounded-lg border bg-foreground p-6 text-orange md:border-orange md:bg-light-brown">
+          <div className="md:background-light flex flex-col items-center rounded-lg border bg-background-light p-6 text-orange md:border-orange">
             <Image priority src={PresentationIcon} alt="PRESENTATION" />
             <h3 className="text-h3-mobile md:text-h3">Presentation</h3>
             <p>
@@ -74,7 +74,7 @@ export default function Page() {
               分鐘的問答時間，讓您有充分的時間展示想法、描述經歷、和與會者交流。
             </p>
           </div>
-          <div className="flex flex-col items-center rounded-lg border bg-foreground p-6 text-red md:border-red md:bg-light-brown">
+          <div className="md:background-light flex flex-col items-center rounded-lg border bg-background-light p-6 text-red md:border-red">
             <Image priority src={EspressoIcon} alt="PRESENTATION" />
             <h3 className="text-h3-mobile md:text-h3">Espresso</h3>
             <p>
@@ -88,7 +88,7 @@ export default function Page() {
         id="submission-topics"
         className="my-4 -mt-[60px] space-y-4 pt-[80px] md:mb-16"
       >
-        <h2 className="text-h2-mobile font-extrabold text-light-blue md:text-h2">
+        <h2 className="text-h2-mobile font-extrabold text-primary md:text-h2">
           投稿主題範例
         </h2>
         <p className="leading-10">
@@ -115,10 +115,10 @@ export default function Page() {
         </p>
       </section>
       <section id="submission-format" className="-mt-[80px] pt-[80px] md:mb-16">
-        <h2 className="text-h2-mobile font-extrabold text-light-blue md:text-h2">
+        <h2 className="text-h2-mobile font-extrabold text-primary md:text-h2">
           投稿方式
         </h2>
-        <h3 className="my-4 text-h3-mobile text-light-blue md:text-h3">
+        <h3 className="my-4 text-h3-mobile text-primary md:text-h3">
           稿件資訊
         </h3>
         <ul className="list-inside list-disc leading-10">
@@ -176,7 +176,7 @@ export default function Page() {
             選填。可以附上程式碼、投影片草稿或任何您認為有助於我們瞭解您議程的附件，請再三確認是否有開啟共用權限。
           </li>
         </ul>
-        <h3 className="my-4 text-h3-mobile text-light-blue md:text-h3">
+        <h3 className="my-4 text-h3-mobile text-primary md:text-h3">
           個人資料
         </h3>
         <p className="mb-2">
@@ -215,7 +215,7 @@ export default function Page() {
         </ul>
       </section>
       <section id="review-process" className="-mt-[80px] pt-[80px] md:mb-16">
-        <h2 className="text-h2-mobile font-extrabold text-light-blue md:text-h2">
+        <h2 className="text-h2-mobile font-extrabold text-primary md:text-h2">
           審稿方式
         </h2>
         <p className="leading-10">
