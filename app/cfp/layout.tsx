b41184d727e7389948019624308e06cd48cb75ba
Dada878 <xx0932399@gmail.com>
2024-11-21 23:45:13 +0800

    refactor: remove unused import of Inter in layout.tsx

--- app/cfp/layout.tsx
+++ app/cfp/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./_components/navbar";
 import Footer from "./_components/footer";
