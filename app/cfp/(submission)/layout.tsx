d373bc83a9b0821a30ef5631a4e1ef9a94e98cfd
Dada878 <xx0932399@gmail.com>
2024-11-21 19:59:39 +0800

    feat: update layout text color to use text-foreground utility

--- app/cfp/(submission)/layout.tsx
+++ app/cfp/(submission)/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex justify-center bg-background text-light-brown">
+    <div className="flex justify-center bg-background text-foreground">
       <div className="max-w-[90%] py-20 md:max-w-[65%]">{children}</div>
     </div>
   );
