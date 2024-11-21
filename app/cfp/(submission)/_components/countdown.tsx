a0bf791f3d3c26eca70979da5dfc8ffe7ff55854
Dada878 <xx0932399@gmail.com>
2024-11-21 20:11:04 +0800

    feat: refactor Countdown component to use new CalendarSvg and update styling for consistency

--- app/cfp/(submission)/_components/countdown.tsx
+++ app/cfp/(submission)/_components/countdown.tsx
@@ -1,11 +1,9 @@
 "use client";
 import { useEffect, useState } from "react";
-import Image from "next/image";
 import Link from "next/link";
 import { motion } from "framer-motion";
 
-import CalendarSvg from "@/public/calender.svg";
-import UpRightArrowSvg from "@/public/north_east.svg";
+import CalendarSvg from "./logos/calendar";
 
 type TimeLeft = {
   days: number;
@@ -56,8 +54,8 @@ export default function CountdownPage() {
 
   return (
     <div className="mx-auto hidden h-[289px] w-full flex-col justify-between md:mb-16 md:flex">
-      <div className="flex h-[200px] w-full flex-col items-center rounded-3xl border border-light-brown px-24 py-5">
-        <h3 className="text-normal tracking-widest text-light-brown">
+      <div className="flex h-[200px] w-full flex-col items-center rounded-3xl border border-foreground px-24 py-5">
+        <h3 className="text-normal tracking-widest text-foreground">
           距 離 投 稿 截 止 還 有 :
         </h3>
         <div className="mt-5 flex w-full justify-between">
@@ -69,11 +67,11 @@ export default function CountdownPage() {
                   initial={{ y: -30, opacity: 0 }}
                   animate={{ y: 0, opacity: 1 }}
                   transition={{ type: "spring", stiffness: 100 }}
-                  className="text-6xl text-light-brown"
+                  className="text-6xl text-foreground"
                 >
                   {formatToTwoDigits(value)}
                 </motion.span>
-                <p className="text-normal font-light text-light-brown">
+                <p className="text-normal font-light text-foreground">
                   {key.toUpperCase()}
                 </p>
               </div>
@@ -82,33 +80,28 @@ export default function CountdownPage() {
         </div>
       </div>
       <div className="flex h-[67px] w-full justify-between">
-        <div className="flex h-full w-[57%] items-center justify-between rounded-2xl border border-light-brown px-5">
-          {/* calender icon */}
-          <Image
-            src={CalendarSvg}
-            width={48}
-            height={48}
-            alt="calendar"
-            className="min-w-[48px]"
-          />
-          <div className="w-full justify-between pl-2 text-light-brown lg:flex lg:pl-12">
-            <p className="font-bold">截止日期</p>
-            <p>即日起至2024年1月21日(六) 23：59</p>
+        <div className="flex h-full w-[57%] items-center justify-between rounded-2xl border border-foreground px-5">
+          <CalendarSvg height={48} width={48} />
+          <div className="w-full justify-between pl-2 text-foreground lg:pl-12 min-[1210px]:flex">
+            <p className="whitespace-nowrap text-nowrap font-bold">截止日期</p>
+            <p className="whitespace-nowrap text-nowrap">
+              2024 年 1 月 21 日（六）23:59
+            </p>
           </div>
         </div>
         <motion.div
           onHoverStart={() => setIsHovered(true)}
           onHoverEnd={() => setIsHovered(false)}
-          className="h-full w-[40%] rounded-2xl bg-light-brown"
+          className="h-full w-[40%] rounded-2xl bg-foreground"
         >
           <Link
             className="flex h-full w-full items-center justify-center"
             href={"#"}
           >
-            <h3 className="flex h-full w-1/2 items-center justify-center text-h3 font-bold text-red">
-              立刻投稿
+            <h3 className="flex h-full items-center justify-center text-xl font-bold text-black">
+              把截止日期加入行事曆
             </h3>
-            <motion.div
+            {/* <motion.div
               animate={{
                 x: !isHovered ? 0 : 10,
                 y: !isHovered ? 0 : -10,
@@ -137,7 +130,7 @@ export default function CountdownPage() {
                 height={40}
                 alt="up_right_arrow"
               />
-            </motion.div>
+            </motion.div> */}
           </Link>
         </motion.div>
       </div>
