4911677d7e3b0c455016a5cfa07b6b700ef25904
Dada878 <xx0932399@gmail.com>
2024-11-21 23:42:21 +0800

    feat: add Google Tag Manager integration and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from "react";
 import { Inter } from "next/font/google";
 import "./cfp/globals.css";
+import { GoogleTagManager } from '@next/third-parties/google';
 
 const inter = Inter({ subsets: ["latin"] });
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-NPVBCDZ" />
       <head>
         <link
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
