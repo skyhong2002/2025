0fa1b8cfc7a346a34342686d0afa961c8986b870
Pizza6inch <pizza6inch@gmail.com>
2024-11-22 11:16:59 +0800

    layout調窄

--- app/cfp/(submission)/layout.tsx
+++ app/cfp/(submission)/layout.tsx
@@ -3,7 +3,9 @@ import { ReactNode } from "react";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex justify-center bg-background text-foreground">
-      <div className="max-w-[90%] py-20 md:max-w-[65%]">{children}</div>
+      <div className="max-w-[90%] py-20 md:max-w-[65%] lg:max-w-[50%]">
+        {children}
+      </div>
     </div>
   );
 }
