7ef9c4732c06e17b4bd6aa39e4372e45a3add5b7
Dada878 <xx0932399@gmail.com>
2024-12-08 15:27:48 +0800

    fix: RWD does not work well on some screen sizes

--- app/cfp/(submission)/layout.tsx
+++ app/cfp/(submission)/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex justify-center bg-background text-foreground">
-      <div className="max-w-[90%] py-20 md:max-w-[65%] lg:max-w-[50%]">
+      <div className="flex max-w-[90%] justify-center py-20 md:max-w-[65%] lg:max-w-[50%]">
         {children}
       </div>
     </div>
