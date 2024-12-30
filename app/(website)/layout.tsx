4d3ad885b202d5a878f18fe744ca2cf725e4afd4
huiwu Zhang <pizza6inch@gmail.com>
2024-12-30 10:55:59 +0800

    feat:add default traffic page

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,16 +1,11 @@
 import { ReactNode } from "react";
-import Navbar from "./_components/navbar";
-import Footer from "./_components/footer";
-import { NavbarProvider } from "@/app/cfp/_components/navbar-context";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <>
-      <NavbarProvider>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </NavbarProvider>
-    </>
+    <div className="flex w-full justify-center bg-background text-foreground">
+      <div className="flex w-full max-w-[90%] justify-center py-20 md:max-w-[80%]">
+        {children}
+      </div>
+    </div>
   );
 }
