e6da00bff7c22c92ae09182d465f6a546896cee7
Kevin Chang <kevin0216@kevin0216.us>
2025-01-05 03:36:07 +0800

    feat: separated GroupInfo and MemberInfo. Also added different icons for website
    feat(test): added LGBTQ+ shadow

--- app/(website)/staff/page.tsx
+++ app/(website)/staff/page.tsx
@@ -4,33 +4,13 @@ import TableOfContent from "@/app/cfp/(submission)/_components/table-of-content"
 // import TableOfContentMobile from "@/app/cfp/(submission)/_components/table-of-content-mobile";
 import staffJson from "./staff.json";
 import Link from "next/link";
-import { motion } from "framer-motion";
+import GroupInfo from "../_components/group_info";
 
 export default function Page() {
   const sections = staffJson.map((group) => ({
     id: group.group,
     title: group.group,
   }));
-  const outside = {
-    notOnHover: {
-      scale: 1,
-      filter: "drop-shadow(0px 0px 0px rgba(201,235,255,70))",
-    },
-    onHover: {
-      scale: 1.2,
-      filter: "drop-shadow(0px 0px 5px rgba(201,235,255,70))",
-    },
-  };
-
-  const pictureBlur = {
-    onHover: { opacity: 1, filter: "blur(0.5em) brightness(0.3)" },
-    notOnHover: { opacity: 1, filter: "blur(0) brightness(1)" },
-  };
-
-  const icon = {
-    onHover: { opacity: 1 },
-    notOnHover: { opacity: 0 },
-  };
 
   return (
     <div className="flex items-start gap-8 py-8 text-normal-mobile md:text-normal">
@@ -64,69 +44,7 @@ export default function Page() {
         </div>
         {/* <TableOfContentMobile theme="light" sections={sections} submitUrl="" /> */}
         {staffJson.map((group) => (
-          <motion.div
-            key={group.group}
-            initial={{ opacity: 0, filter: "blur(0.5em)" }}
-            whileInView={{
-              opacity: 1,
-              filter: "blur(0)",
-              transition: { duration: 0.25 },
-            }}
-            viewport={{ once: true }}
-          >
-            <section id={group.group} key={group.group} className="mb-8">
-              <h2 className="text-h2-mobile font-bold md:text-h2">
-                {group.group}
-              </h2>
-              <p>{group.description}</p>
-              <div className="mt-6 grid grid-cols-[repeat(auto-fill,minmax(78px,1fr))] gap-6 md:gap-12 md:gap-y-8">
-                {group.staff.map((member) => (
-                  <div key={member.name} id={member.name}>
-                    <div className="flex flex-col items-center gap-4 text-center">
-                      {member.website ? (
-                        <motion.button
-                          initial="notOnHover"
-                          whileHover="onHover"
-                          variants={outside}
-                          onClick={() => window.open(member.website, "_blank")}
-                          className="relative h-20 w-20 overflow-hidden rounded-full"
-                        >
-                          <motion.img
-                            variants={pictureBlur}
-                            src={`https://gravatar.com/avatar/${member.email}?s=512&d=https://sitcon.org/2022/imgs/deafult_avatar.jpg&r=g`}
-                            className="h-full w-full"
-                          />
-                          <motion.span
-                            variants={icon}
-                            className="material-icons absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-white"
-                            style={{ fontSize: "30px" }}
-                          >
-                            open_in_new
-                          </motion.span>
-                        </motion.button>
-                      ) : (
-                        <motion.img
-                          initial="notOnHover"
-                          whileHover="onHover"
-                          variants={outside}
-                          src={`https://gravatar.com/avatar/${member.email}?s=512&d=https://sitcon.org/2022/imgs/deafult_avatar.jpg&r=g`}
-                          className="h-20 w-20 rounded-full"
-                        />
-                      )}
-                      <div>
-                        <h3 className="text-[16px] font-bold md:text-[19px]">
-                          {member.name}
-                        </h3>
-                        <p className="text-[12px] md:text-[14px]">
-                          {member.role}
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </section>
-          </motion.div>
+          <GroupInfo key={group.group} group={group} />
         ))}
       </div>
       <TableOfContent theme="light" sections={sections} submitUrl="" />
