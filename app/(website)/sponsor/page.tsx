1e12687b1b9da8338157703f2616a9651522648c
nelsonGX <hi@nelsongx.com>
2025-01-05 20:01:08 +0800

    feat: add main host section

--- app/(website)/sponsor/page.tsx
+++ app/(website)/sponsor/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <>
+    <div className="space-y-32">
       {/* 支持我們 */}
       <div className="items-start text-normal-mobile md:text-normal">
         <h1 className="text-h1 font-bold">支持我們</h1>
@@ -50,7 +50,47 @@ export default function Page() {
           contact@sitcon.org 與我們聯繫，商酌更多的合作方式！
           扶持資訊教育，讓我們一同使更多的臺灣學子能在舞台上展現自我、相互成長！
         </p>
+        <div className="py-[22.5px]" />
+        <div className="flex space-x-7">
+          <button className="h-[60px] w-[153px] rounded-full bg-primary text-black">
+            個人贊助
+          </button>
+          <button className="h-[60px] w-[154px] rounded-full bg-primary text-black">
+            索取贊助徵求書
+          </button>
+        </div>
       </div>
-    </>
+
+      {/* 主辦單位 */}
+      <div className="items-center text-normal-mobile md:text-normal">
+        <h1 className="text-center text-h1 font-bold">主辦單位</h1>
+        <div className="py-[22.5px]" />
+        <h2 className="text-center text-h2 font-bold">SITCON 學生計算機年會</h2>
+        <div className="py-[34.5px]" />
+        <div className="flex justify-center">
+          <Image
+            src="https://placehold.co/385x109?text=SITCON%20Logo%20385x109_4"
+            alt="SITCON Logo"
+            width={385}
+            height={109}
+          />
+        </div>
+        <div className="py-[37px]" />
+        <div className="flex justify-center">
+          <p className="max-w-[685px]">
+            SITCON 學生計算機年會 (Students’ Information Technology Conference)
+            是一個由各校同學於網路串聯發起，鼓勵自發學習、切磋成長的學生社群。
+            <br />
+            <br />
+            因為熱愛資訊領域、知道尋找興趣相近夥伴的不容易，籌備團隊從 2013
+            年以來，便致力於連結對資訊科技相關領域有興趣的學生們，透過每年三月的研討會分享彼此所學，在每週的定期聚交流精進。發展至今，SITCON
+            除了是東亞最大的學生資訊社群與研討會，各地參與者也藉由夏令營、一小時學程式
+            (Hour of
+            Code)、黑客松等不同的面向活動，推廣創新、實作、教學相長、以及開源貢獻的精神與理念；在台灣與海外各地，都有
+            SITCON 參與者的身影。
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
