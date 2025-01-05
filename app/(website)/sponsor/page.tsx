8a016dd06ddf2a94838b741aa81c9056edd66f9f
nelsonGX <hi@nelsongx.com>
2025-01-05 21:58:30 +0800

    feat: add hyperlinks

--- app/(website)/sponsor/page.tsx
+++ app/(website)/sponsor/page.tsx
@@ -42,9 +42,28 @@ export default function Page() {
         </div>
         <p className="max-w-[685px] pb-[55px] leading-loose">
           而維繫學生們的資訊交流平台，除了有賴志工們的努力及貢獻，亦十分需要在資金上的支持。
-          教育不只在課本裡，如果您也認同 SITCON
-          的理念，歡迎點擊下方「個人贊助」或「索取贊助徵求書」按鈕以了解詳細的合作方案，或是寄信至
-          contact@sitcon.org 與我們聯繫，商酌更多的合作方式！
+          教育不只在課本裡，如果您也認同 SITCON 的理念，歡迎點擊下方「
+          <a
+            href="https://ocf.neticrm.tw/"
+            className="underline underline-offset-2"
+          >
+            個人贊助
+          </a>
+          」或「
+          <a
+            href="https://i.sitcon.org/2022cfs-form"
+            className="underline underline-offset-2"
+          >
+            索取贊助徵求書
+          </a>
+          」按鈕以了解詳細的合作方案，或是寄信至{" "}
+          <a
+            href="mailto:contact@sitcon.org"
+            className="underline underline-offset-2"
+          >
+            contact@sitcon.org
+          </a>{" "}
+          與我們聯繫，商酌更多的合作方式！
           扶持資訊教育，讓我們一同使更多的臺灣學子能在舞台上展現自我、相互成長！
         </p>
         <div className="flex space-x-7">
