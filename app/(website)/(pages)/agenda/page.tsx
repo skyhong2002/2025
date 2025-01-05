791be2e355ecd2d31d535bf484f26b1a8ba4651f
星月なるこ <projectcsj@gmail.com>
2025-01-05 20:15:07 +0800

    feat: agenda page add desktop-agenda and mobile-agenda
    Add import

--- app/(website)/(pages)/agenda/page.tsx
+++ app/(website)/(pages)/agenda/page.tsx
@@ -1,5 +1,16 @@
-import AgendaPage from "@/app/(website)/_components/AgendaPage";
+import DesktopAgenda from "@/app/(website)/_components/desktop-agenda";
+import MobileAgenda from "@/app/(website)/_components/mobile-agenda";
 
 export default function Page() {
-  return <AgendaPage />;
+  return (
+    <>
+      <h1 className="text-h1-mobile text-white md:text-h1">議程表</h1>
+      <div className="hidden md:block">
+        <DesktopAgenda />
+      </div>
+      <div className="md:hidden">
+        <MobileAgenda />
+      </div>
+    </>
+  );
 }
