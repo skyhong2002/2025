207fd369e406bcbce08eacea65671aa713c68c51
Dada878 <xx0932399@gmail.com>
2025-01-07 05:33:35 +0800

    fix: comment out sponsor links in navbar and footer; adjust layout padding and update transportation options

--- app/(website)/(pages)/tickets/page.tsx
+++ app/(website)/(pages)/tickets/page.tsx
@@ -5,7 +5,8 @@ export default function Page() {
     <div className="flex flex-col items-start justify-center gap-24">
       <section id="RegisterInfo" className="flex flex-col items-start gap-4">
         <h1 className="text-h1-mobile font-bold md:text-h1">報名資訊</h1>
-        <p className="w-full leading-[200%] md:w-[70%]">
+        <p className="w-full leading-[200%] md:w-[80%]">
+        <p className="w-full leading-[200%] md:w-[80%]">
           SITCON
           年會堅持不收費原則，秉持著「降低學生參與門檻」的理念，預算全數由個人／企業贊助或是政府／教育單位協助，若您也認同
           SITCON 的理念，歡迎支持我們。
@@ -29,12 +30,18 @@ export default function Page() {
           </div>
 
           <div className="flex flex-col items-center justify-center py-4 md:w-4/12 md:py-10">
-            <ul className="w-[80%] list-inside list-disc text-normal-mobile leading-[200%] md:text-start md:text-normal">
-              <li className="listItem mt-4 md:mt-0 md:pl-[-22px]">
-                第一波售票：01/12 20:00 ～01/25 23:59
+            <ul className="text-center text-normal-mobile leading-[200%] md:list-inside md:list-disc md:text-start md:text-normal">
+              <li className="listItem mt-4 !pl-0 md:mt-0 md:pl-[-22px]">
+                第一波售票：
+                <br />
+                2025/01/12 20:00～2025/01/25 23:59
+                2025/01/12 20:00～2025/01/25 23:59
               </li>
-              <li className="listItem mt-4 md:mt-0 md:pl-[-22px]">
-                第二波售票：02/09 20:00 ～02/22 23:59
+              <li className="listItem mt-4 !pl-0 md:mt-0 md:pl-[-22px]">
+                第二波售票：
+                <br />
+                2025/02/09 20:00～2025/02/22 23:59
+                2025/02/09 20:00～2025/02/22 23:59
               </li>
             </ul>
             <LinkButton
@@ -51,12 +58,14 @@ export default function Page() {
             <div className="dashLine" />
           </div>
           <div className="mt-4 flex items-center justify-center md:w-2/12">
-            <LinkButton
-              link={"https://sitcon.kktix.cc/events/sitcon2025"}
+            <Link
+              href={"https://sitcon.kktix.cc/events/sitcon2025"}
               target="_blank"
-              text="立即取得"
-              className="ml-[-4px] md:mr-4 md:w-full"
-            />
+            >
+              <button className="ml-[-4px] mr-4 flex h-[60px] w-full items-center justify-center rounded-full bg-primary p-4 text-normal text-black">
+                KKTIX 報名連結
+              </button>
+            </Link>
           </div>
           <div className="right-0 z-10 mr-[-1px] box-border hidden aspect-1/2 rounded-bl-full rounded-tl-full border border-l-white border-r-background bg-background md:block md:w-1/12" />
         </div>
@@ -76,12 +85,9 @@ export default function Page() {
               <li className="listItem mt-4 md:mt-0 md:pl-[-22px]">
                 資格限制：住在偏鄉或離島、海外地區的學生。
               </li>
-              <li className="listItem mt-4 md:mt-0 md:pl-[-22px]">
-                先到先審制，報名者需在 2025/01/21 23:59 前填寫申請表單。
-                先到先審制，報名者需在 2025/01/21 23:59 前填寫申請表單。
-              </li>
               <li className="listItem mt-4 !pl-0 md:mt-0 md:pl-[-22px]">
-                申請資格及更詳細的申請說明，請參考 SITCON 2024 遠道而來票。
+                先到先審制，報名者需在 2025/01/21 23:59 前填寫申請表單。
+                先到先審制，報名者需在 2025/01/21 23:59 前填寫申請表單。
               </li>
             </ul>
           </div>
@@ -89,12 +95,15 @@ export default function Page() {
             <div className="dashLine" />
           </div>
           <div className="mt-4 flex items-center justify-center md:w-2/12">
-            <LinkButton
-              link={"https://hackmd.io/@SITCON/H18WacxQ1x"}
+            <Link
+              className="link"
+              href={"https://hackmd.io/@SITCON/H18WacxQ1x"}
               target="_blank"
-              text="立即取得"
-              className="ml-[-4px] md:mr-4 md:w-full"
-            />
+            >
+              <button className="ml-[-4px] mr-4 flex h-[60px] w-[190px] items-center justify-center rounded-full bg-primary p-4 text-normal text-black md:w-full">
+                立即取得
+              </button>
+            </Link>
           </div>
 
           <div className="right-0 z-10 mr-[-1px] box-border hidden aspect-1/2 rounded-bl-full rounded-tl-full border border-l-white border-r-background bg-background md:block md:w-1/12" />
@@ -115,7 +124,7 @@ export default function Page() {
               <li className="listItem mt-4 md:mt-0 md:pl-[-22px]">
                 資格限制：對開源專案有貢獻者。
               </li>
-              <li className="listItem mt-4 md:mt-0 md:pl-[-22px]">
+              <li className="listItem mt-4 !pl-0 md:mt-0 md:pl-[-22px]">
                 在 2025/01/21 23:59 前申請且通過者，有機會可以搶到 SITCON 2025
                 開源貢獻票邀請碼。申請表單、申請資格及更詳細的申請說明，請參考
                 <Link
@@ -135,7 +144,7 @@ export default function Page() {
                   開源貢獻者快速通關
                 </Link>
               </li>
-              <li className="listItem mt-4 md:mt-0 md:pl-[-22px]">
+              <li className="listItem mt-4 !pl-0 md:mt-0 md:pl-[-22px]">
                 關於歷年來 OSCVPass 申請通過者的專案列表，請參考{" "}
                 <Link
                   className="link"
@@ -165,12 +174,11 @@ export default function Page() {
             <div className="dashLine" />
           </div>
           <div className="mt-4 flex items-center justify-center md:w-2/12">
-            <LinkButton
-              link={"https://ocf.tw/p/oscvpass/"}
-              target="_blank"
-              text="立即取得"
-              className="ml-[-4px] md:mr-4 md:w-full"
-            />
+            <Link href={"https://ocf.tw/p/oscvpass/"} target="_blank">
+              <button className="ml-[-4px] mr-4 flex h-[60px] w-[190px] items-center justify-center rounded-full bg-primary p-4 text-normal text-black md:w-full">
+                立即取得
+              </button>
+            </Link>
           </div>
 
           <div className="right-0 z-10 mr-[-1px] box-border hidden aspect-1/2 rounded-bl-full rounded-tl-full border border-l-white border-r-background bg-background md:block md:w-1/12" />
@@ -178,12 +186,16 @@ export default function Page() {
       </section>
       <section
         id="SubmitInfo"
-        className="flex w-full flex-col items-start gap-4 leading-[200%] md:w-[70%]"
+        className="flex w-full flex-col items-start gap-4 leading-[200%] md:w-[80%]"
+        className="flex w-full flex-col items-start gap-4 leading-[200%] md:w-[80%]"
       >
         <h1 className="mb-8 text-h1-mobile font-bold md:text-h1">投稿資訊</h1>
         <p className="">
           SITCON
-          期望成為學生展現自己的舞台，如果你有想要與大家分享的經驗、技術或專案，我們歡迎任何與資訊或電腦軟硬體相關的講題。
+          作為學生展現自己的舞台，我們特別期待以學生為主體的稿件，例如：從學生角度出發的經驗分享、技術分享，專題研究成果、獨立研究甘苦談，或探討學生相關議題等等。
+          不論是探討技術或輕鬆的分享，任何與資訊科技、電腦技術相關的講題，我們都非常歡迎。
+          作為學生展現自己的舞台，我們特別期待以學生為主體的稿件，例如：從學生角度出發的經驗分享、技術分享，專題研究成果、獨立研究甘苦談，或探討學生相關議題等等。
+          不論是探討技術或輕鬆的分享，任何與資訊科技、電腦技術相關的講題，我們都非常歡迎。
         </p>
         <p>歡迎於 2024/01/24 前用以下形式的投搞</p>
         <li>海報投稿：以靜態海報展出學生個人或團隊專案研究作品。</li>
@@ -191,13 +203,28 @@ export default function Page() {
         <li>
           一般議程：10 分鐘與 40 分鐘的議程，帶給會眾知識的洗禮與思想的壯遊。
         </li>
+        <p>歡迎於 2024/01/20 前用以下形式的投搞（已截止）</p>
         <li>
-          開放式議程：40
-          分鐘的議程，不限定進行方式，歡迎任何非傳統、實現性質的形式進行此場議程。
+          一般議程：SITCON 2025 邀請您將您的精彩經歷以 Presentation 與 Espresso
+          兩種形式呈現。Presentation 想邀請您用 40
+          分鐘深入分享一個專案、技術或有趣的經歷，而 Espresso 則想挑戰您在 10
+          分鐘內精練的介紹一個主題的能力。無論是技術或經驗的分享，都歡迎投稿一般議程。
+          一般議程：SITCON 2025 邀請您將您的精彩經歷以 Presentation 與 Espresso
+          兩種形式呈現。Presentation 想邀請您用 40
+          分鐘深入分享一個專案、技術或有趣的經歷，而 Espresso 則想挑戰您在 10
+          分鐘內精練的介紹一個主題的能力。無論是技術或經驗的分享，都歡迎投稿一般議程。
         </li>
-        <button className="mt-6 flex h-[60px] w-[190px] items-center justify-center self-center rounded-full bg-primary p-4 text-normal text-black md:w-[153px] md:self-start">
-          了解更多
-        </button>
+        <li>
+          開放式議程：如果您想呈現的主題更加獨特、創新，想跳脫講者對聽眾單向陳述式的演講，那開放式議程會是你的最好選擇！想帶一個實作的工作坊、和其他與會者一同討論特定議題、邀請社群的夥伴彼此分享經驗，或想以任何形式增加議程多元與互動性的稿件，都歡迎來挑戰開放式議程。今年開放式議程也提供
+          40 或 90 分鐘兩種時長，提供您更彈性的呈現方式。
+          開放式議程：如果您想呈現的主題更加獨特、創新，想跳脫講者對聽眾單向陳述式的演講，那開放式議程會是你的最好選擇！想帶一個實作的工作坊、和其他與會者一同討論特定議題、邀請社群的夥伴彼此分享經驗，或想以任何形式增加議程多元與互動性的稿件，都歡迎來挑戰開放式議程。今年開放式議程也提供
+          40 或 90 分鐘兩種時長，提供您更彈性的呈現方式。
+        </li>
+        <Link href={"https://sitcon.org/2025/cfp"} target="_blank">
+          <button className="mt-6 flex h-[60px] w-[190px] items-center justify-center self-center rounded-full bg-primary p-4 text-normal text-black md:w-[153px] md:self-start">
+            了解更多
+          </button>
+        </Link>
       </section>
     </div>
   );
