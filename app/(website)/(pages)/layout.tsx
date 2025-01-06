85c79838b1ed9ea946f3f08f335f21e12adac557
Dada878 <xx0932399@gmail.com>
2025-01-06 19:48:00 +0800

    feat: complete home page

--- app/(website)/(pages)/layout.tsx
+++ app/(website)/(pages)/layout.tsx
@@ -1,4 +1,7 @@
 import { ReactNode } from "react";
+import Navbar from "./_components/navbar";
+import Footer from "./_components/footer";
+import { NavbarProvider } from "@/app/cfp/_components/navbar-context";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
