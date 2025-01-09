5d89de256de8c85145950fa1b68eb8d44a7fb16e
Dada878 <xx0932399@gmail.com>
2025-01-09 22:55:00 +0800

    feat: restore generateMetadata function in layout for improved SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,47 @@ import { GoogleTagManager } from "@next/third-parties/google";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { Metadata } from "next";
 config.autoAddCss = false;
 
 const inter = Inter({ subsets: ["latin"] });
+
+export async function generateMetadata(): Promise<Metadata> {
+  const image = "https://sitcon.org/2025/og.jpg";
+  const title = "SITCON 2025";
+  const description =
+    "學生計算機年會（Students’ Information Technology Conference）自 2012 年發起，以學生為本、由學生自發舉辦，長期投身學生資訊教育與推廣開源精神，鼓勵學生們成為實踐者，貢獻程式碼、打造並部署服務、透過實際行動推動專案，最後將這些結晶分享出去，讓更多人能關注你認為重要的議題，打造更緊密的社群。";
+  const metadata: Metadata = {
+    title,
+    authors: [{ name: "SITCON" }],
+    keywords: "SITCON, SITCON 2025",
+    description,
+    metadataBase: new URL("https://sitcon.org/2025/"),
+    openGraph: {
+      type: "website",
+      locale: "zh_TW",
+      url: "https://sitcon.org/2025/cfp/",
+      title,
+      description,
+      siteName: "SITCON",
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      creator: "@sitcontw",
+      site: "@sitcontw",
+      card: "summary_large_image",
+      images: image,
+      title,
+      description,
+    },
+  };
+  return metadata;
+}
+
 export default function Layout({ children }: { children: ReactNode }) {
   const jsonLd = {
     "@context": "https://schema.org",
