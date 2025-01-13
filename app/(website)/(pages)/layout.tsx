cf2acc23b2cbbeb0906585825550c16133be9174
Dada878 <xx0932399@gmail.com>
2025-01-14 02:46:26 +0800

    feat: add horizontal padding to layout for improved spacing

--- app/(website)/(pages)/layout.tsx
+++ app/(website)/(pages)/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex w-full justify-center bg-background text-foreground">
-      <div className="flex w-full max-w-[90%] flex-col justify-center py-40 lg:max-w-[840px] desktop:max-w-[1120px]">
+      <div className="flex w-full max-w-[90%] flex-col justify-center px-4 py-40 lg:max-w-[840px] desktop:max-w-[1120px]">
         {children}
       </div>
     </div>
