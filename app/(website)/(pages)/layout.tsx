7e20ae94566b8de82cd5e503840ce7d18887c9fb
Dada878 <xx0932399@gmail.com>
2025-01-14 02:21:59 +0800

    feat: adjust layout max-width for better responsiveness

--- app/(website)/(pages)/layout.tsx
+++ app/(website)/(pages)/layout.tsx
@@ -6,7 +6,7 @@ import { NavbarProvider } from "@/app/cfp/_components/navbar-context";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex w-full justify-center bg-background text-foreground">
-      <div className="flex w-full max-w-[90%] justify-center py-40 md:max-w-[80%]">
+      <div className="flex w-full max-w-[90%] flex-col justify-center py-40 lg:max-w-[840px] desktop:max-w-[1120px]">
         {children}
       </div>
     </div>
