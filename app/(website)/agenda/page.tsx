d475f5928470c42085ef4e90c716c0afe57b5e4d
星月なるこ <projectcsj@gmail.com>
2025-01-14 21:38:56 +0800

    feat: add margin top to agenda component in agenda page

--- app/(website)/agenda/page.tsx
+++ app/(website)/agenda/page.tsx
@@ -5,11 +5,13 @@ export default function Page() {
   return (
     <>
       <h1 className="text-h1-mobile text-white md:text-h1">議程表</h1>
-      <div className="hidden md:block">
-        <DesktopAgenda />
-      </div>
-      <div className="md:hidden">
-        <MobileAgenda />
+      <div className="mt-4">
+        <div className="hidden md:block">
+          <DesktopAgenda />
+        </div>
+        <div className="md:hidden">
+          <MobileAgenda />
+        </div>
       </div>
     </>
   );
