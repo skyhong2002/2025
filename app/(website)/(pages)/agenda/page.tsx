ed1a70b025dd926ecf68d2740d286b39d97fc181
huiwu Zhang <pizza6inch@gmail.com>
2025-01-15 17:10:38 +0800

    add:Session detail popup

--- app/(website)/(pages)/agenda/page.tsx
+++ app/(website)/(pages)/agenda/page.tsx
@@ -1,18 +1,5 @@
-import DesktopAgenda from "@/app/(website)/_components/desktop-agenda";
-import MobileAgenda from "@/app/(website)/_components/mobile-agenda";
+import AgendaPage from "@/app/(website)/_components/AgendaPage";
 
 export default function Page() {
-  return (
-    <>
-      <h1 className="text-h1-mobile text-white md:text-h1">年會議程表</h1>
-      <div className="mt-4">
-        <div className="hidden md:block">
-          <DesktopAgenda />
-        </div>
-        <div className="md:hidden">
-          <MobileAgenda />
-        </div>
-      </div>
-    </>
-  );
+  return <AgendaPage />;
 }
