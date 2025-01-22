bb6923ab84b5b9baa0e3c80b17f714a09b202fb3
Dada878 <xx0932399@gmail.com>
2025-01-22 22:24:30 +0800

    fix: adjust padding in layout components for improved spacing

--- app/(website)/(pages)/layout.tsx
+++ app/(website)/(pages)/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex w-full justify-center bg-background text-foreground">
-      <div className="flex w-full max-w-[90%] flex-col justify-center px-4 py-20 lg:max-w-[840px] desktop:max-w-[1120px] desktop:py-40">
+      <div className="flex w-full max-w-[90%] flex-col justify-center px-4 py-28 md:py-40 lg:max-w-[840px] desktop:max-w-[1120px]">
         {children}
       </div>
     </div>
