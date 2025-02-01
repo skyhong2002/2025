48b49fffe692ff03049fb829bf7e29b56c6455c1
Dada878 <xx0932399@gmail.com>
2025-02-02 05:09:42 +0800

    feat: improve session popup layout & styling in mobile device

--- app/(website)/(pages)/_components/SessionPopup.tsx
+++ app/(website)/(pages)/_components/SessionPopup.tsx
@@ -68,132 +68,138 @@ const SessionPopup = ({ openSessionId }: { openSessionId: string | null }) => {
           animate={{ opacity: 1, scale: 1, y: "-50%", x: "-50%" }}
           transition={{ duration: 0.5, ease: "easeInOut", type: "spring" }}
           exit={{ opacity: 0, scale: 0.8, y: "-50%", x: "-50%" }}
-          className="fixed left-[50%] top-[50%] z-50 h-[80vh] w-full translate-x-[-50%] translate-y-[-50%] overflow-y-scroll rounded-3xl bg-[#ffffff] p-6 text-black md:w-[90vw] md:p-10"
+          className="fixed left-[50%] top-[50%] z-50 h-[90%] w-[90%] translate-x-[-50%] translate-y-[-50%] overflow-y-scroll rounded-3xl bg-[#ffffff] p-6 text-black md:h-[80vh] md:w-[90vw] md:p-10 desktop:max-w-[1220px]"
         >
-          <div className="flex flex-col gap-2">
-            <div className="flex h-[24px] w-[24px] cursor-pointer items-center justify-center self-end rounded-full bg-[#304878]">
-              <Link href="/agenda" scroll={false}>
-                <button className="flex items-center justify-center !font-bold text-[#ffffff]">
-                  <span className="material-symbols-outlined !text-[16px]">
-                    close
-                  </span>
-                </button>
-              </Link>
-            </div>
-            <div className="flex max-w-full flex-col gap-3 md:max-w-[60%]">
-              <div className="flex items-center gap-2">
-                <h2 className="self-start text-[20px] font-medium md:self-center">
-                  {getSessionName(session.type)}
-                </h2>
-                <div className="flex flex-row gap-2">
-                  {session.tags.map((tag) => (
-                    <span
-                      className="rounded-full bg-[#D9D9D9] px-3 py-1.5 text-[12px]"
-                      key={tag}
-                    >
-                      #{tag}
+          <div className="relative">
+            <div className="flex flex-col gap-2">
+              <div className="sticky right-0 top-0 -mb-8 flex h-[24px] w-[24px] cursor-pointer items-center justify-center self-end rounded-full bg-[#304878] md:-translate-y-4 md:translate-x-4">
+                <Link href="/agenda" scroll={false}>
+                  <button className="flex items-center justify-center !font-bold text-[#ffffff]">
+                    <span className="material-symbols-outlined !text-[16px]">
+                      close
                     </span>
-                  ))}
-                </div>
+                  </button>
+                </Link>
               </div>
-              <h1 className="text-h2-mobile font-bold md:text-h2">
-                {session.zh.title}
-              </h1>
-              <div className="flex flex-row items-center gap-4">
-                <div className="flex flex-row items-center gap-2">
-                  <span className="material-symbols-outlined !text-[24px] !font-bold">
-                    schedule
-                  </span>
-                  <p className="text-[16px] font-bold">
-                    {getTimeText(session.start, session.end)}
-                  </p>
-                </div>
-                <div className="flex flex-row items-center gap-2">
-                  <span className="material-symbols-outlined !text-[24px] !font-bold">
-                    location_on
-                  </span>
-                  <p className="text-[16px] font-bold">{session.room}</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-10">
-              <div className="flex w-full flex-col gap-10 md:flex-row">
-                <div className="flex w-full flex-col gap-2 md:w-[65%]">
-                  <div className="flex flex-row">
-                    <div className="w-[50%]">目標聽眾</div>
-                    <div className="w-[50%]">先備知識</div>
+              <div className="flex max-w-full flex-col gap-3 md:max-w-[60%]">
+                <div className="flex items-center gap-2">
+                  <h2 className="self-start text-[20px] font-medium md:self-center">
+                    {getSessionName(session.type)}
+                  </h2>
+                  <div className="flex flex-row gap-2">
+                    {session.tags.map((tag) => (
+                      <span
+                        className="rounded-full bg-[#D9D9D9] px-3 py-1.5 text-[12px]"
+                        key={tag}
+                      >
+                        #{tag}
+                      </span>
+                    ))}
                   </div>
-                  <h2 className="text-h3-mobile font-bold">議程介紹：　</h2>
-                  <Markdown className="space-y-4 leading-7 max-md:text-sm">
-                    {session.zh.description}
-                  </Markdown>
                 </div>
-                <div className="flex w-full flex-col gap-3 md:w-[35%]">
-                  {session.record && typeof session.record === "string" && (
-                    <YouTubeEmbed
-                      style="border-radius: 20px;"
-                      videoid={session.record.replace("https://youtu.be/", "")}
-                    />
-                  )}
-                  <div className="flex flex-row justify-between gap-2">
-                    <button
-                      onClick={() => {
-                        if (session.qa) window.open(session.qa, "_blank");
-                      }}
-                      className="flex-grow text-nowrap rounded-full bg-[#B9D3E6] px-3 py-2 text-center text-[16px] font-bold transition-colors hover:bg-[#A1C7E0]"
-                    >
-                      即時提問
-                    </button>
-                    <button
-                      onClick={() => {
-                        if (session.slide) window.open(session.slide, "_blank");
-                      }}
-                      className="flex-grow text-nowrap rounded-full bg-[#B9D3E6] px-3 py-2 text-center text-[16px] font-bold transition-colors hover:bg-[#A1C7E0]"
-                    >
-                      簡報連結
-                    </button>
-                    <button
-                      onClick={() => {
-                        if (session.co_write)
-                          window.open(session.co_write, "_blank");
-                      }}
-                      className="flex-grow text-nowrap rounded-full bg-[#B9D3E6] px-3 py-2 text-center text-[16px] font-bold transition-colors hover:bg-[#A1C7E0]"
-                    >
-                      共筆連結
-                    </button>
+                <h1 className="text-h2-mobile font-bold md:text-h2">
+                  {session.zh.title}
+                </h1>
+                <div className="flex flex-row items-center gap-4">
+                  <div className="flex flex-row items-center gap-2">
+                    <span className="material-symbols-outlined !text-[24px] !font-bold">
+                      schedule
+                    </span>
+                    <p className="text-[16px] font-bold">
+                      {getTimeText(session.start, session.end)}
+                    </p>
+                  </div>
+                  <div className="flex flex-row items-center gap-2">
+                    <span className="material-symbols-outlined !text-[24px] !font-bold">
+                      location_on
+                    </span>
+                    <p className="text-[16px] font-bold">{session.room}</p>
                   </div>
                 </div>
               </div>
 
-              {speakers &&
-                speakers.map((speaker) => (
-                  <div
-                    key={speaker?.id}
-                    className="flex w-full flex-col gap-10 md:flex-row"
-                  >
-                    <div className="flex flex-col gap-4 md:w-[65%]">
-                      <div className="text-2xl font-bold">
-                        {speaker?.zh.name}
-                      </div>
-                      <Markdown className="space-y-4 leading-7 max-md:text-sm">
-                        {speaker?.zh.bio}
-                      </Markdown>
+              <div className="flex flex-col gap-10">
+                <div className="flex w-full flex-col gap-10 md:flex-row">
+                  <div className="flex w-full flex-col gap-2 md:w-[65%]">
+                    <div className="flex flex-row">
+                      <div className="w-[50%]">目標聽眾</div>
+                      <div className="w-[50%]">先備知識</div>
                     </div>
-                    {speaker?.avatar && (
-                      <div className="md:w-[35%]">
-                        <Image
-                          layout="responsive"
-                          alt="speaker"
-                          style={{ borderRadius: "20px" }}
-                          src={speaker?.avatar}
-                          width={100}
-                          height={100}
-                        />
-                      </div>
-                    )}
+                    <h2 className="text-h3-mobile font-bold">議程介紹：　</h2>
+                    <Markdown className="space-y-4 leading-7 max-md:text-sm">
+                      {session.zh.description}
+                    </Markdown>
                   </div>
-                ))}
+                  <div className="flex w-full flex-col gap-3 md:w-[35%]">
+                    {session.record && typeof session.record === "string" && (
+                      <YouTubeEmbed
+                        style="border-radius: 20px;"
+                        videoid={session.record.replace(
+                          "https://youtu.be/",
+                          "",
+                        )}
+                      />
+                    )}
+                    <div className="flex flex-row justify-between gap-2">
+                      <button
+                        onClick={() => {
+                          if (session.qa) window.open(session.qa, "_blank");
+                        }}
+                        className="flex-grow text-nowrap rounded-full bg-[#B9D3E6] px-3 py-2 text-center text-[16px] font-bold transition-colors hover:bg-[#A1C7E0]"
+                      >
+                        即時提問
+                      </button>
+                      <button
+                        onClick={() => {
+                          if (session.slide)
+                            window.open(session.slide, "_blank");
+                        }}
+                        className="flex-grow text-nowrap rounded-full bg-[#B9D3E6] px-3 py-2 text-center text-[16px] font-bold transition-colors hover:bg-[#A1C7E0]"
+                      >
+                        簡報連結
+                      </button>
+                      <button
+                        onClick={() => {
+                          if (session.co_write)
+                            window.open(session.co_write, "_blank");
+                        }}
+                        className="flex-grow text-nowrap rounded-full bg-[#B9D3E6] px-3 py-2 text-center text-[16px] font-bold transition-colors hover:bg-[#A1C7E0]"
+                      >
+                        共筆連結
+                      </button>
+                    </div>
+                  </div>
+                </div>
+
+                {speakers &&
+                  speakers.map((speaker) => (
+                    <div
+                      key={speaker?.id}
+                      className="flex w-full flex-col gap-10 md:flex-row"
+                    >
+                      <div className="flex flex-col gap-4 md:w-[65%]">
+                        <div className="text-2xl font-bold">
+                          {speaker?.zh.name}
+                        </div>
+                        <Markdown className="space-y-4 leading-7 max-md:text-sm">
+                          {speaker?.zh.bio}
+                        </Markdown>
+                      </div>
+                      {speaker?.avatar && (
+                        <div className="md:w-[35%]">
+                          <Image
+                            layout="responsive"
+                            alt="speaker"
+                            style={{ borderRadius: "20px" }}
+                            src={speaker?.avatar}
+                            width={100}
+                            height={100}
+                          />
+                        </div>
+                      )}
+                    </div>
+                  ))}
+              </div>
             </div>
           </div>
         </motion.div>
