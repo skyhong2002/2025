21ac8523154c2eeed3a853e0735bda1ce4a74556
Dada878 <xx0932399@gmail.com>
2025-02-02 16:47:18 +0800

    fix: adjust sticky header position in AgendaTable component for improved layout

--- app/(website)/(pages)/_components/AgendaTable.tsx
+++ app/(website)/(pages)/_components/AgendaTable.tsx
@@ -47,11 +47,11 @@ export default function DesktopAgenda() {
       }}
     >
       {/* row1 empty R2 R0 R1 R3 S  */}
-      <div className="sticky top-[120px] z-20 border-b border-b-white border-opacity-30 bg-background py-2 text-center text-[24px] font-bold"></div>
+      <div className="sticky top-[70px] z-20 border-b border-b-white border-opacity-30 bg-background py-2 text-center text-[24px] font-bold"></div>
       {data.rooms.map((rooms, index) => (
         <div
           key={index}
-          className="sticky top-[120px] z-20 border-b border-b-white border-opacity-30 bg-background py-2 text-center text-[24px] font-bold"
+          className="sticky top-[70px] z-20 border-b border-b-white border-opacity-30 bg-background py-2 text-center text-[24px] font-bold"
         >
           {rooms.id}
         </div>
