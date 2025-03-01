629df0a54e4048fffe69bfa402e4bd0ec1654380
arnoldsky-yu <99588albert@gmail.com>
2025-03-01 22:22:47 +0800

    fix: update venue data

--- app/(website)/(pages)/venue/venueData.ts
+++ app/(website)/(pages)/venue/venueData.ts
@@ -1,172 +1,212 @@
 export default {
   2: [
     {
-      number: "01",
+      number: "0",
       title: "咖啡廳",
-      description:
-        "想與來自四面八方的會眾進行交流嗎？還是想繼續討論剛剛精彩的議程嗎？抑或是想要有個地方好好休息呢？那麼歡迎來到 SITCON 咖啡廳。在這裡，我們提供悠閒的環境、舒適的空間，以及不可或缺的——咖啡，讓各個需求的會眾都能在這裡找到屬於自己的小天地，無論你是想與人聊天、討論，或是在一整天豐富的行程中想稍微喘口氣，甚至是在這裡腦力激盪出一項專案，我們都歡迎！",
-    },
-    {
-      number: "01",
-      title: "靜態海報展",
-      description:
-        "這是 SITCON 2024 的新嘗試！我們提供學生們一個展現自我的舞台，讓大家可以透過靜態海報展發表自己做的研究，同時也能和志同道合的同學當面交流想法，碰撞出知識的火花！歡迎投稿靜態海報展，一起加入這場學術派對！",
+      description:"想與來自四面八方的會眾進行交流嗎？還是想繼續討論剛剛精彩的議程嗎？抑或是想要有個地方好好休息呢？那麼歡迎來到 SITCON 咖啡廳。在這裡，我們提供悠閒的環境、舒適的空間，以及不可或缺的——咖啡，讓各個需求的會眾都能在這裡找到屬於自己的小天地，無論你是想與人聊天、討論，或是在一整天豐富的行程中想稍微喘口氣，甚至是在這裡腦力激盪出一項專案，我們都歡迎！"
     },
   ],
   3: [
     {
       number: "1",
-      title: "2024 第十二屆梅竹黑客松",
+      title: "島島阿學學習社群",
+      url: "https://www.daoedu.tw/",
       description:
-        "梅竹黑客松在今年邁入第十二屆，我們定期在清大與交大兩校間辦理黑客松相關活動，致力搭起學生與企業之間的橋樑，學生運用企業資源並發揮潛力回應企業命題，同時企業方更提供實習機會，能讓學生進一步實踐創意發想。我們期待可以創造一個平台，讓任何人都可以享受想法的誕生與實作。",
+        "島島阿學由百位熱血公民以集體智慧組成，包含教育工作者、學生、工程師、設計師等跨年齡跨領域的夥伴。2020年來，致力以科技與社群創建自主學習生態圈，解決民主教育中推行「自主學習、民主社群」的困難，如找不到目標、資源、人脈、難自律等，目前提供自主學習資源網站（找優質資源、學伴、老師、揪團等），以及線上交流社群（Discord）、學習馬拉松（自主學習陪伴計畫）等活動。讓我們一起在島島阿學慢慢學不用急，在互助共好中成就彼此。（IG：https://www.instagram.com/daodao_edu/）",
     },
     {
       number: "2",
-      title: "美商超微半導體股份有限公司台灣分公司",
-      image: "amd.png",
+      title: "g0v Jothon 零時政府揪松團",
+      url: "https://linktr.ee/g0vjothon",
       description:
-        "50 多年來，AMD（NASDAQ：AMD）推動創新高效能運算、繪圖及視覺技術，建構遊戲、高臨場感平台與資料中心等重要領域。全球數十億的消費者、世界 500 強企業以及尖端科學研究機構皆仰賴 AMD 的技術來改善生活、工作及娛樂。AMD 員工致力於研發領先的高效能與自行調適產品，不斷突破技術的極限。",
-      url: "https://www.amd.com/zh-hant",
+        "g0v 零時政府揪松團 Jothon 是籌辦 g0v 黑客松和基礎松的工作小組 🙂 2012 年 12 月起兩個月舉辦一次百人公民科技黑客松 ❤️‍🔥 推動「零時小學校：新世代數位公民實踐」，以及撰寫 g0v 公民科技專案與社群手冊 💪 大松小松活動訊息彙整網址 https://linktr.ee/g0vjothon",
     },
     {
       number: "3",
-      title: "SITCON 學生計算機年會",
+      title: "Cofacts 真的假的 訊息回報機器人與查證協作社群",
+      url: "https://cofacts.tw",
       description:
-        "自 2012 年發起，以學生為本、由學生自發舉辦，長期投身學生資訊教育與推廣開源精神，希望引領更多學子踏入資訊的殿堂，更冀望所有對資訊有興趣的學生，能夠在年會裏齊聚一堂，彼此激盪、傳承、啟發，達到「學以致用、教學相長」的實際展現。",
-    },
-    {
-      number: "4",
-      title: "COSCUP 開源人年會",
-      description:
-        "COSCUP 是由台灣開放原始碼社群聯合推動的年度研討會，起源於 2006 年，是台灣自由軟體運動 (FOSSM) 重要的推動者之一。活動包括有講座、攤位、社團同樂會等，除了邀請國際的重量級演講者之外，台灣本土的自由軟體推動者也經常在此發表演說，會議的發起人、工作人員與講者都是志願參與的志工。COSCUP 的宗旨在於提供一個聯結開放原始碼開發者、使用者與推廣者的平台。希望藉由每年一度的研討會，來推動自由及開放原始碼軟體 (FLOSS)。",
+        "在 LINE 上收到可疑的轉傳訊息，就先傳到 @cofacts 來看看網友怎麼說吧！這是個 2016 年持續至今的開源專案，蒐集 LINE 上的可疑訊息，並由網友擔任查核協作者，在 https://cofacts.tw 上面撰寫查核回應。查核成果以 CC BY-SA 釋出，讓研究者、智庫、媒體與有興趣的任何人取用。",
     },
     {
       number: "5",
-      title: "開放文化基金會",
+      title: "Ruby Taiwan",
+      url: "https://www.facebook.com/RubyJamTW",
       description:
-        "OCF 希望能夠藉由法人組織的力量，協助台灣的開放文化相關社群，包含開放原始碼軟體、開放資料、開放政府三個主要領域的發展和應用。期許能將開源自由的精神，更廣泛地應用在各個領域。",
+        "Ruby Taiwan 成立於 2008 年10 月，主要針對 Ruby 程式語言及軟體開發，進行業界的交流及分享，提昇開發人員的技術深度與廣度.",
     },
     {
       number: "6",
-      title: "PyCon TW",
+      title: "JSDC",
+      url: "https://jsdc.tw/",
       description:
-        "PyCon，亦即 Python 年會，是全球 Python 社群年度盛會。PyCon TW 今年九月即將在高雄科工館舉辦第十二年的台灣 Python 年會。PyCon TW 旨在聚集台灣各領域應用 Python 的人，交流彼此的經驗心得，分享最新的技術發展，並親身體驗社群獨有的熱情。",
+        "JavaScript 開發者年會 (JavaScript Developer Conference，簡稱 JSDC) 是台灣最大的 JavaScript 年度性技術研討會，旨在提供台灣中高階 JavaScript 技術人才與世界最新 JavaScript 相關技術討論與分享。最初從 2011 年底開始，由多個台灣開發者社群共同發起，近年各式開發者社群蓬勃發展 Front-End Developer、 Angular.tw、 ReactJS.tw、Vue.tw、 JavaScript.tw 等新興社群陸續成立，我們期許 JSDC 能成為每年網頁技術開發者的中高階技術交流活動，整合來自獨立開發者、企業、組織及機構的科技力量，並致力於知識全球化，讓參與者重新認識 JavaScript.\nFacebook 粉絲專頁：https://www.facebook.com/JSDC.TW",
     },
     {
       number: "7",
-      title: "帕亞科技股份有限公司 & 臺科大營建系",
-      image: "paia-and-ntust-cce.png",
+      title: "sciwork",
+      url: "https://sciwork.dev/",
       description:
-        "帕亞科技目前主力產品為 PAIA 遊戲 AI 競賽平台，我們打造了一個遊戲情境，讓使用者可以上傳各自的 AI 在平台上競賽，也提供積木式的開發工具，讓國中小學生可以輕易使用，學習 AI。\n\n臺科大營建系資訊組以促進資通訊科技於土木營建與防災工程的全生命週期導入與應用為目標，朝向智慧營建及永續土木工程等方向發展。",
-      urls: [
-        {
-          title: "帕亞科技股份有限公司",
-          url: "https://www.paia-arena.com/",
-        },
-        {
-          title: "臺科大營建系",
-          url: "https://www.ct.ntust.edu.tw/ct/",
-        },
-      ],
+        "sciwork is a community for researchers and engineers to share and discuss computer code for scientific, numerical, and engineering work. We believe in the power of openness, and use open source as a means to advance software development for computational sciences.\n\nhttps://sciwork.dev/",
     },
     {
       number: "8",
-      title: "中部高中電資社團聯合會議",
+      title: "MozTW 社群 / 摩茲工寮",
+      url: "moztw.org",
       description:
-        "中部高中電資社團聯合會議（簡稱中電會）成立於 2021 年，是由一群熱愛資訊科技和社團活動的高中職生所組成，而主要目的在於整合中部資訊科技資源，推廣資訊相關的課程。\n\n在這學期我們舉辦了數場免費專業領域課程、國中生課程、每月主題課程、以及聯合寒訓。目前我們正在對社群進行大更新，希望能創造一個活躍的開放討論空間與資源中心。\n\n中電會以資訊安全起家，我們目前預計在 SITCON 中展示使用陀螺儀竊取密碼的互動活動，期待能推廣我們的課程及社群。\n\n以下是我們的官網和 IG，若有任何問題都歡迎隨時聯繫我。\n\nhttps://scaict.org\n\nhttps://www.instagram.com/scaict.tw",
+        "MozTW 是台灣 Mozilla 愛好者的實體社群，設有網站提供相關軟體下載等服務，並負責維護 Mozilla 大部分軟體的正體中文版本與語言套件的在地化。摩茲工寮是 MozTW 為開放社群與活動所設計，也由社群共同管理的空間.",
+    },
+    {
+      number: "9",
+      title: "中部高中電資社團聯合會議",
+      url: "https://scaict.org",
+      description:
+        "中部高中電資社團聯合會議（簡稱中電會）成立於 2021 年，是由一群熱愛資訊科技和社團活動的高中職生所組成，而主要目的在於整合中部資訊科技資源，推廣資訊相關的課程。\n\n在去年我們舉辦了數場免費專業領域課程、國中生課程、每月主題課程、以及聯合寒訓。目前我們正在對社群進行大更新，希望能創造一個活躍的開放討論空間與資源中心。\n\n以下是我們的官網和 IG，若有任何問題都歡迎隨時聯繫我們.\n\nhttps://scaict.org\n\nhttps://www.instagram.com/scaict.tw",
+    },
+    {
+      number: "10",
+      title: "SCIST 南台灣學生資訊社群",
+      url: "https://scist.org",
+      description:
+        "SCIST 南臺灣學生資訊社群\n成立於 2019 年，至今已邁入第五屆，SCIST 由一群對資訊充滿熱忱的高中生組成。致力於整合南部資訊資源，透過集結南部各高中資訊社，並提供免費資源，供高中學子學習與交流。同時，SCIST 也籌辦寒訓，並長期投入舉辦為期一整學年度的資安與演算法培訓課程。我們期許自身能持續成為資訊教育中的永續綠洲，為更多熱愛技術與學習的學子提供資源與發展機會.",
+    },
+    {
+      number: "11",
+      title: "SCINT 北臺灣學生資訊社群",
+      url: "https://scint.org",
+      description:
+        "SCINT 北臺灣學生資訊社群創立於 2023，擁有超過 20 所北、中、南部高中職合作社團，是北部最大的高中職生資訊社群，我們希望成為社團間的膠水，解決北部社團小圈圈問題，並推廣程式、資安、專案開發、硬體開發等不同領域的課程.\n\nInstagram: https://www.instagram.com/scint.tw/\nFacebook 粉專: https://www.facebook.com/scint.tw/\nDiscord 伺服器: https://discord.gg/7sgwT3a77Z",
     },
   ],
   4: [
     {
-      number: "9",
-      title: "Dcard 狄卡科技",
-      image: "dcard.png",
-      description:
-        "Dcard 是全台最大匿名社群， 從台灣走向世界，每月不重複訪客高達 2,200 萬，發展出廣告、電商、跨國市場等領域，持續探索新型商業模式，邁向世界級網路公司前進。",
-      url: "https://join.dcard.today/",
-    },
-    {
-      number: "10",
-      title: "曉數碼 Akatsuki Taiwan Inc.",
-      image: "akatsuki.png",
-      description:
-        "曉數碼為 Akatsuki Inc. 於 2014 年在台灣設立的子公司，以「與創作者共鳴，娛樂全世界」為使命，致力於開發、營運各種能躍動人心的遊戲。曉數碼重視在地化品質並放眼國際市場，團隊中聚集來自各地的成員，包含台、日、韓、英、法、德、西與印度等多語言人才。我們亦積極打造幸福職場，以此支持成員們盡情發揮創意，使其發光發熱，共同成長。",
-      url: "https://bit.ly/3bwhylS",
-    },
-    {
-      number: "11",
-      title: "紀念品攤位",
-      description: "",
-    },
-    {
       number: "12",
-      title: "LINE",
-      image: "line.png",
+      title: "Cloud Native Taiwan User Group",
+      url: "https://cloudnative.tw",
       description:
-        "總部位於日本的 LINE 以「拉近你我的距離」為企業使命，透過多元的行動服務與內容，致力將人們、資訊與社會緊密連結。自 2011 年 6 月以通訊軟體的型態推出，現更推展其多元的全球生態系，並將觸角延伸至人工智慧、金融科技等領域，積極實現「智慧入口」的願景，2021 年 3 月，LINE 與日本規模最大的網路科技集團之一 Z Holdings 正式整併，未來也將持續提供用戶享有最即時、便利的行動生活。",
-      url: "https://techblog.lycorp.co.jp/zh-hant/search?q=intern",
+        "Cloud Native Taiwan User Group (CNTUG) 是一個致力於主要推廣 Cloud Native 雲端原生相關技術並同時涵蓋 OpenInfra Foundation 範疇等技術的臺灣在地化社群。作為臺灣在地的 Kubernetes 和眾多 CNCF 專案使用者，該社群提供了一個技術交流平台，聚集了各行各業的使用者、開發者和傳教者，分享最新的雲端原生技術和實踐經驗，推動臺灣在雲端原生領域的發展.",
     },
     {
       number: "13",
-      title: "趨勢科技",
-      image: "trendmicro.png",
+      title: "WordPress 台灣社群",
+      url: "https://www.facebook.com/groups/wordpresstw",
       description:
-        "趨勢科技為資訊安全解提供決方案全球領導廠商，致力建立一個安全的資訊交換世界。為資料中心、雲端工作負載、網路、端點裝置提供多層式安全防護。",
-      url: "https://careers.trendmicro.tw",
+        "WordPress台灣社群是一個聚集熱愛網站開發、設計及內容創作的愛好者與專業人士的活躍社群，致力於推廣WordPress在台灣的使用與應用。我們透過舉辦線上與線下活動，如WordPress Meetup、工作坊及年會WordCamp，分享最新技術、交流經驗，並建立緊密的合作網絡。無論您是剛接觸WordPress的新手，還是資深的開發者，這裡都提供友善且支持性的環境，讓每位成員都能找到學習與成長的機會.",
     },
     {
       number: "14",
-      title: "HKCOTA x 香港開源年會",
+      title: "Hacking Thursday",
+      url: "https://www.hackingthursday.org",
       description:
-        "《香港創意開放科技協會》成立於 2014 年，致力向學生及在職人士推廣及教育開放文化、標準，自由及開放軟體、硬體、資料，以及 Creative Commons 《共享創意》概念及授權條款。而至今已經舉辦過 10 屆的《香港開源年會》既是一個年會，也是一個空間、平台，匯聚著香港和海外開源社群、開發者，以及各個界別的朋友，一同在年會中分享開發或使用開源、自由軟體，以及大家一路走來在開源自由領域的經驗。",
+        "Hacking Thursday 是由幾位居住於台北地區的自由軟體/開放原碼開發者所發起，每週四晚上會於特定咖啡店聚會。以非會議形式、交換並實做各自提出的想法，輕鬆的會議過程以禮貌、謙遜與尊重的互信態度接納並鼓勵概念發想、發起新計畫、並從開發者的協同開發與經驗分享中互相學習。聚會十分的隨性，不時產生下列分散的行為：\n\n討論 網站設計, 電腦網路, 程式設計以及作業系統 …\n交流 軟硬體工具和使用技巧\n八卦\n聚會與會者都是聽眾，但同時也可能是閃電秀的講者.",
     },
     {
       number: "15",
-      title: "島島阿學學習社群",
+      title: "Ubuntu-TW",
+      url: "https://www.facebook.com/ubuntu.tw",
       description:
-        "島島阿學是由親師生以集體智慧共創的民主學習社群。邀請學習者一同解決民主教育兩大元素「自主學習、民主社群」的困難，如找不到目標、資源、學伴等。目前提供可共編的學習資源平台，邀學習者共享資源。今年上半年也將上架找學伴、老師、揪團的功能。一起慢慢學不用急，在互助共好中成就彼此。",
+        "Ubuntu-TW 成立於 2005 年 6 月，是由數名愛好者發起，以推廣 Ubuntu 為目標的台灣在地社群。除了每年固定舉辦的 Release Party 以及在台灣各開源研討會擺攤展示之外，我們的成員也時常出沒於各大開源社團，而所舉辦的 Release Party 也一直都有其他 Linux 發行版愛好者的參與。除了社群活動之外，我們的成員也有參與 Ubuntu 及其他發行版、開源軟體的 L10N，編篡文件，以及在網上替人解惑。歡迎各路新手高手、舊雨新知，加入 Ubuntu-TW ，跟我們一起推廣 Ubuntu、Linux 及開源文化！\n\n網站/連結：\n* 官方網站/論壇：http://www.ubuntu-tw.org/\n* FB 社團：https://www.facebook.com/groups/ubuntu.zh.hant/\n* FB 粉絲頁：https://www.facebook.com/ubuntu.tw\n* IRC：Libera.Chat #ubuntu-tw\n* Telegram: https://t.me/x_ubuntu_taiwan_community",
     },
     {
       number: "16",
-      title: "g0v 零時小學校",
+      title: "開放學生自治社群 SOGA Commuity",
+      url: "https://g0v.hackmd.io/@Ted-Lu/r1WKJ58ua" ,
       description:
-        "零時小學校致力於將 g0v 零時政府（公民科技社群）自發性運用科技做專案解決社會問題的經驗與精神，轉化為專題導向教學模式與學習資源，將數位公民素養向下扎根。透過與學校合作課程、提供 70 堂以上數位公民線上課程、舉辦營隊與專案孵化競賽等方式，陪伴 2000 位以上師生，近四年來共收到 123 件公民科技專案，其中 74 件作品係由國高中生及大學生提出。",
+        "Student Open Governance Associated Community. 關心國內各級學校學生自治之開放治理發展現況。我們致力於探討如何透過開放資料、民主協作及科技工具，提升學生自治的透明度、效率與影響力，讓學生的聲音能夠更被看見，並促成更好的教育環境與校園政策。\n\n我們自 [SITCON 2024 的開放式議程](https://sitcon.org/2024/agenda/999db2/) 建立此社群，將在年會當天屆滿一週年 🎉🎉🎉\n\n我們歡迎：\n- 目前在各校自治組織活躍的學生\n- 願意分享開放學生自治相關案例的同學\n- 對開放治理有興趣的開發者 & 設計師\n- 關心開放學生自治的所有人！\n\n過往活動&社群共筆： https://g0v.hackmd.io/@Ted-Lu/r1WKJ58ua",
     },
     {
       number: "17",
-      title: "MozTW 社群 / 摩茲工寮",
+      title: "PyCon TW",
+      url: "https://tw.pycon.org/2025/zh-hant",
       description:
-        "MozTW 是台灣 Mozilla 愛好者的實體社群，設有網站提供相關軟體下載等服務，並負責維護 Mozilla 大部分軟體的正體中文版本與語言套件的在地化。摩茲工寮是 MozTW 為開放社群與活動所設計，也由社群共同管理的空間。",
+        "https://tw.pycon.org/2025/zh-hant/about",
     },
     {
       number: "18",
-      title: "台灣敏捷協會",
+      title: "開放文化基金會",
+      url: "ocf.tw",
       description:
-        "台灣敏捷協會 Agile Community Taiwan 由一群敏捷志工與業界先驅發起，是內政部正式登記的社團法人協會，致力於提供並推廣敏捷價值、思維與務實導向理念，藉由推動敏捷文化引擎以驅動轉化、改善台灣這塊土地上的人們的生活、學習、社群、產業與經濟。\n\nhttps://www.act.club.tw/",
+        "開放文化基金會（Open Culture Foundation，簡稱 OCF）\n是一個非營利性的組織。透過推廣開放科技和跨界合作，在臺灣持續的銜繫科技社群與其他公／私領域，來促成開放共創保障數位人權、支持透明涵融的數位公民社會。\n\n自 2014 年創立以來，我們以法人做為組織形式，支持臺灣 40 多個開放科技社群，在過程中，不僅是持續性的推廣開放科技概念，我們也開始參與政策倡議，為眾人的數位人權發聲，進而創建了一個在臺灣發展開放科技的基地。這些成果並非由開放文化基金會獨立實現，而是透過科技社群和公民夥伴一起促使臺灣的數位/網路環境變得更加公開、透明和公眾參與 — 不論這樣的參與是以個人、社群或是組織為名義.",
     },
     {
       number: "19",
-      title: "台灣不分系學生聯合協會",
+      title: "COSCUP",
+      url: "https://coscup.org/2025",
       description:
-        "不分系是近幾年於台灣大專院校所開始嘗試之實驗教育，透過鬆綁大專院校制度，藉由校級或是院級學士的方式讓同學能夠順利以其他科系的方式畢業。而這個制度雖然帶來同學選課的自由，同時帶來同學對於自己未來的迷惘，在大學路上努力的過程可能能夠被看見，但也可能會停留在原地，也沒有身旁的朋友能夠幫助。\n\n台灣不分系學生聯合協會是一個正進行籌組的協會，現有成員多以成大不分系，部分其它科系為主，未來期望在籌組階段可以廣招全台各地對於不分系有興趣的學生，無論是否是不分系學生均可加入。我們期待能夠作為一個能夠幫助大家接軌，聯合各校不分系系學會合作，舉辦全國性不分系相關團體活動，透過「競賽」和「團隊媒合」的平台，作為全台灣自由度最高的科系，以及想法最天馬行空的科系，能夠透過各個大學的特色與各個同學個人不分系的經驗結合，讓大家能夠獲得一個想法付諸實現的平台，以及結交新同儕的一個資訊交流互通平台。",
+        "COSCUP 是由台灣開放原始碼社群聯合推動的年度研討會，起源於 2006 年，是台灣自由軟體運動 (FOSSM) 重要的推動者之一。活動包括有講座、攤位、社團同樂會等，除了邀請國際的重量級演講者之外，台灣本土的自由軟體推動者也經常在此發表演說，會議的發起人、工作人員與講者都是志願參與的志工。COSCUP 的宗旨在於提供一個聯結開放原始碼開發者、使用者與推廣者的平台。希望藉由每年一度的研討會，來推動自由及開放原始碼軟體 (FLOSS).",
     },
     {
       number: "20",
-      title: "StarUp 人才孵化器",
+      title: "紀念品攤位",
       description:
-        "StarUp 人才孵化器致力打造台灣校園的創新創業生態系，培育下一個改變台灣、改變世界的未來領袖。\n\n我們透過經營學生社群、人才培訓與投創基金，聚集、培育並投資積極成長的大學生。\n\n我們舉辦創新創業的社群活動，匯集「不躺平的年輕人」，成立2年以來，現已舉辦 44 場活動、觸及 1500+ 人次，私域社群人數累積達 404 人，並且與三間大學社團，組成「BeTheChange 創創聯盟」，共享資源與連結。\n\n此外，我們也著重培育幹部具備商業思維與職場技能，舉辦創業家、經理人的培訓計畫，幫助認證人才進入企業見習、實習，縮短學用落差，現已培育 37 位夥伴，成功媒合 22 人次的優秀人才進入職場，並有2位透過內推，畢業即獲得正職工作。\n\n最後，我們於 2024 年開始，與炙星投創合作，成立學生投創基金，管理 300 萬資金專注投資學生創業團隊，期待挖掘、培跑下一個改變世界的學生創業家。 \n\n我們是StarUp人才孵化器，透過社群、培訓與投資的力量，陪伴台灣有想法的大學生一同成長，成為新時代的創新創業領袖，歡迎你加入我們！\n\n更多資料歡迎閱覽以下簡報 https://reurl.cc/67dXRb",
+      "我們推出了獨一無二的紀念品，有融合主視覺圖案的悠遊卡和吊飾、精心設計的鍵帽和紀念 T-shirt。別忘了來逛逛紀念品攤位，挑選屬於您的特別回憶！",
     },
     {
       number: "21",
-      title: "Ruby Taiwan",
+      title: "LINE",
+      url: "https://techblog.lycorp.co.jp/zh-hant/search?q=intern",
       description:
-        "Ruby Taiwan 社群成立於 2008 年 10 月，主要是針對 Ruby 程式語言及軟體開發，進行業界的交流及分享，提昇開發人員的技術深度與廣度",
-    },
+      "總部位於日本的LINE以「拉近你我的距離」\n\n為企業使命，透過多元的行動服務與內容，致力將人們、資訊與社會緊密連結。自2011年6月以通訊軟體的型態推出，現更推展其多元的全球生態系，並將觸角延伸至人工智慧、金融科技等領域，積極實現「智慧入口」的願景，2021年3月，LINE與日本規模最大的網路科技集團之一Z Holdings 正式整併，未來也將持續提供用戶享有最即時、便利的行動生活。",    },
     {
       number: "22",
-      title: "Ubuntu-TW",
+      title: "NTNU CSIE Universe",
+      url: "https://ntnucsie.info/",
       description:
-        "Ubuntu-TW 成立於 2005 年 6 月，是由數名愛好者發起，以推廣 Ubuntu 為目標的台灣在地社群。除了每年固定舉辦的 Release Party 以及在台灣各開源研討會擺攤展示之外，我們的成員也時常出沒於各大開源社團，而所舉辦的 Release Party 也一直都有其他 Linux 發行版愛好者的參與。除了社群活動之外，我們的成員也有參與 Ubuntu 及其他發行版、開源軟體的 L10N，編篡文件，以及在網上替人解惑。歡迎各路新手高手、舊雨新知，加入 Ubuntu-TW ，跟我們一起推廣 Ubuntu、Linux 及開源文化！",
+        "NTNU CSIE Universe 是一個以師大資工學生為主的 Discord 群組。群組用途以溝通系上重要事項、社交、休閒為主，然而也有許多對資訊議題有熱忱的大神，在群組內熱絡進行技術交流，是個相當活躍的資工社群。\n\n師大資工系學會，是一個由師大資工學生組成的學生自治組織。系學會經常舉辦許多系上活動，其中最為著名的便是師大資工黑客松與 game jam。透過這些活動，讓學生能夠提升短時間製作專題的能力，也可以豐富學生的作品集。\n\n師大學生開發者社群 (GDG on Campus: NTNU) 由學生自主經營，透過工作坊、黑客松、社群聚會等活動，讓大家深入學習和實作 Google 技術與其他前沿技術，如 AI、雲端與各類開源專案。我們鼓勵成員彼此協作、與業界人士互動交流，不只培養扎實的技能與跨領域視野，也在過程中建立深厚的人際連結，共同推動創新與社群發展.\n\n師大資工營（NTNU CSIE CAMP）起源於原資訊教育學系主辦之「暑期電腦營」，至今已有 35 年歷史，其最初理念為辦理提供參與者瞭解有關電腦使用方面之技能。如今，師大資訊工程學系（資工系）將繼續傳承該理念，於每年的暑假舉辦面向國高中生的「資工營」，讓參與的學員通過別具一格的教學方式學習走在前沿的科技與技術，體會工程師由一行行的代碼中細心編織出的，屬於他們的每一個夢想.",
+    },
+    {
+      number: "23",
+      title: "逢甲大學黑客社",
+      url: "https://www.facebook.com/share/15a4AQHPZK/?mibextid=wwXIfr",
+      description:
+        "黑客社成立的目的除了培養出專業的資安人才以外，也同時是為了推廣「黑客精神」，所謂的「黑客精神」是指致力於鑽研並且精進技術的同時也能互相討論以及分享知識。\n以下是我們的 linktree，歡迎來認識我們！\nhttps://linktr.ee/fcuhackersir",
+    },
+    {
+      number: "24",
+      title: "Google Developer Groups On Campus",
+      url: "https://gdg.tw/about/gdsc/",
+      description:
+        "Google Developer Groups on Campus (GDG on Campus) 是一個由Google支持的學生社群，旨在通過學生與專業開發人員聯繫，促進學生對 Google 開發人員技術的學習和應用，並為學生提供與技術專家互動和學習的機會。 GDG on Campus 在全球有超過 100 個國家/地區的 1000 多個社群。GDG on Campus 是學生們學習和分享技術的理想社群，並且能夠與技術行業的其他開發人員和專家建立聯繫.\n全台社群專頁索引：https://gdg.tw/chapters/",
+    },
+    {
+      number: "25",
+      title: "HackIt",
+      url: "https://scrapyard.hackit.tw/",
+      description:
+        "HackIt是一個學習型組織，強調不斷成長、反思與改進為核心價值。團隊由一群充滿熱情的青少生主導，鼓勵自主學習、互相合作，共同打造一個開放、積極的環境。我們鼓勵每位成員提出創意並共同實現，彼此協作，迎接挑戰，並推動不斷的自我突破。https://www.facebook.com/profile.php?id=61567019608090",
+    },
+    {
+      number: "26",
+      title: "Taiwan Kotlin User Group",
+      url: "http://taiwan-kotlin-user-group.github.io/",
+      description:
+        "Taiwan Kotlin User Group 是一個讓大家可以討論與交流 Kotlin 程式語言各種使用經驗，舉辦相關活動的社群。如果對 Kotlin 有興趣，想要多瞭解一些，歡迎來我們的社群一起聚會！\nhttps://www.facebook.com/kotlintwn",
+    },
+    {
+      number: "27",
+      title: "網路遊民探險中：Taiwan Digital Rogue",
+      url: "https://www.meetup.com/taiwan-digital-rogue/",
+      description:
+        "Taiwan Digital Rogue 是推廣數位遊牧生活的台灣社群，聚焦於遠端工作、生態圈交流與技能提升，打造自由且有連結的生活方式.",
+    },
+    {
+      number: "28",
+      title: "台灣數位遊牧者協會",
+      url: "https://dna.org.tw",
+      description:
+        "致力於推動台灣的數位遊牧生態，連結政府、民間企業與校園資源，讓台灣成為適合數位遊牧的地方，也讓大家更了解數位遊牧.",
+    },
+    {
+      number: "29",
+      title: "海報展",
+      description:
+      "我們提供學生們一個展現自我的舞台，讓大家可以透過靜態海報展發表自己做的研究，同時也能和志同道合的同學當面交流想法，碰撞出知識的火花！歡迎投稿靜態海報展，一起加入這場學術派對！",    },
+    {
+      number: "30",
+      title: "冰淇淋",
+      description:
+        "🍦冰冰冰冰冰🍦",
     },
   ],
 };
+
