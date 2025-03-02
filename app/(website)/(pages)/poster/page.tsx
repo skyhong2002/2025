fdb73ccca18a58a9f5fc02803a9d4186af560812
nelsonGX <hi@nelsongx.com>
2025-03-02 21:39:37 +0800

    refactor: refactor data

--- app/(website)/(pages)/poster/page.tsx
+++ app/(website)/(pages)/poster/page.tsx
@@ -1,33 +1,26 @@
 "use client";
 
-import { Pickaxe } from "lucide-react";
-import { motion } from "framer-motion";
+import Image from "next/image";
+import posterData from "./posterData";
 
 export default function PosterPage() {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center gap-6">
-      <motion.div
-        initial={{ rotate: 0 }}
-        animate={{ rotate: [0, 22, 0], x: [-20, -10, -20], y: [0, -10, 0] }}
-        transition={{ duration: 1.5, repeat: Infinity, ease: "easeInOut" }}
-        className="origin-bottom-left"
-      >
-        <Pickaxe className="size-36" strokeWidth={1.3} />
-      </motion.div>
-      <motion.p
-        initial={{ x: 0, y: 0 }}
-        animate={{
-          scale: [1, 1, 1.1, 1, 1],
-        }}
-        transition={{
-          duration: 1.5,
-          repeat: Infinity,
-          ease: "easeInOut",
-        }}
-        className="text-xl"
-      >
-        此頁面正在建置中...
-      </motion.p>
+    <div>
+      <h1 className="pb-16 text-h1 font-bold text-[#FFFFFF]">靜態海報展</h1>
+      <div className="grid grid-cols-1 justify-center gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {posterData.map((poster) => (
+          <div key={poster.title}>
+            <Image
+              src={`/poster/${poster.image}`}
+              alt={poster.title}
+              width={400}
+              height={10}
+              className="rounded-lg"
+            />
+            <h3 className="pt-4 text-h3 text-[#FFFFFF]">{poster.title}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
