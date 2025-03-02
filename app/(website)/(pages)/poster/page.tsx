f12351b69267e914264458ad62c9f42a69333ebe
nelsonGX <hi@nelsongx.com>
2025-03-02 21:55:59 +0800

    feat: add animations when hover

--- app/(website)/(pages)/poster/page.tsx
+++ app/(website)/(pages)/poster/page.tsx
@@ -7,9 +7,12 @@ export default function PosterPage() {
   return (
     <div>
       <h1 className="pb-16 text-h1 font-bold text-[#FFFFFF]">靜態海報展</h1>
-      <div className="grid grid-cols-1 justify-center gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 justify-center gap-4 gap-y-12 md:grid-cols-2 lg:grid-cols-3">
         {posterData.map((poster) => (
-          <div key={poster.title}>
+          <div
+            key={poster.title}
+            className="transition-all duration-300 hover:scale-105 hover:rounded-xl hover:border hover:border-sky-300 hover:p-4"
+          >
             <Image
               src={`/poster/${poster.image}`}
               alt={poster.title}
@@ -17,7 +20,19 @@ export default function PosterPage() {
               height={10}
               className="rounded-lg"
             />
-            <h3 className="pt-4 text-h3 text-[#FFFFFF]">{poster.title}</h3>
+            <h3 className="truncate pt-4 text-xl text-[#FFFFFF]">
+              {poster.title}
+            </h3>
+            <div className="flex items-center space-x-2 pt-2">
+              <Image
+                src={`/poster/${poster.avatars[0]}`}
+                alt={poster.author}
+                width={32}
+                height={32}
+                className="rounded-full"
+              />
+              <p className="text-zinc-300">{poster.author}</p>
+            </div>
           </div>
         ))}
       </div>
