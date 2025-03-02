5e3252311d1d3c5673cb4c559c2e953b4eb45027
Denny Huang <denny0223@gmail.com>
2025-03-02 14:16:33 +0800

    Revert "fix: hide btn in session popup"
    This reverts commit 658e8507d1b8550bc0961ac1f5bca3e6be4f9aed.

--- app/(website)/(pages)/_components/SessionPopup.tsx
+++ app/(website)/(pages)/_components/SessionPopup.tsx
@@ -181,6 +181,34 @@ const SessionPopup = ({ openSessionId }: { openSessionId: string | null }) => {
                         )}
                       />
                     )}
+                    <div className="flex flex-row justify-between gap-2">
+                      <button
+                        onClick={() => {
+                          if (session.qa) window.open(session.qa, "_blank");
+                        }}
+                        className="flex-grow text-nowrap rounded-full bg-[#B9D3E6] px-3 py-2 text-center text-[16px] font-bold transition-colors hover:bg-[#A1C7E0]"
+                      >
+                        即時提問
+                      </button>
+                      <button
+                        onClick={() => {
+                          if (session.slide)
+                            window.open(session.slide, "_blank");
+                        }}
+                        className="flex-grow text-nowrap rounded-full bg-[#B9D3E6] px-3 py-2 text-center text-[16px] font-bold transition-colors hover:bg-[#A1C7E0]"
+                      >
+                        簡報連結
+                      </button>
+                      <button
+                        onClick={() => {
+                          if (session.co_write)
+                            window.open(session.co_write, "_blank");
+                        }}
+                        className="flex-grow text-nowrap rounded-full bg-[#B9D3E6] px-3 py-2 text-center text-[16px] font-bold transition-colors hover:bg-[#A1C7E0]"
+                      >
+                        共筆連結
+                      </button>
+                    </div>
                   </div>
                 </div>
 
